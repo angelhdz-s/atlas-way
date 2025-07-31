d80b6d07d425cd49aa8e1a3324d1bac3e00bed55
AngelHernandes <L20260224@matamoros.tecnm.mx>
2025-07-31 08:35:00 -0500

    Add header login button

--- src/modules/landing/components/Header.tsx
+++ src/modules/landing/components/Header.tsx
@@ -5,7 +5,7 @@ import { ToggleTheme } from "@/modules/globals/components/ToggleTheme";
 export function Header({ className = "" }: { className?: string }) {
 	return (
 		<header
-			className={`ld-main-fg w-full h-12 px-2 md:px-4 lg:px-0 ld-sec-bg ${className}`}
+			className={`ld-main-fg w-full h-14 px-2 md:px-4 lg:px-0 ld-sec-bg ${className}`}
 		>
 			<main className="flex items-center justify-between gap-4 max-w-5xl mx-auto h-full ">
 				<aside className="">
@@ -26,8 +26,14 @@ export function Header({ className = "" }: { className?: string }) {
 						</li>
 					</ul>
 				</nav>
-				<div className="flex items-center">
-					<ToggleTheme />
+				<div className="flex items-center gap-2">
+					<ToggleTheme className="btn-md" />
+					<a
+						href="/login"
+						className="rounded-full btn-md btn-primary py-1.5 animate-fade animate-delay-500"
+					>
+						Log In
+					</a>
 				</div>
 			</main>
 		</header>
