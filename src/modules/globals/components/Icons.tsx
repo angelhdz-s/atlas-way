286b7550375a9b210aea45a836eb57fde0c1f6e6
AngelHernandes <L20260224@matamoros.tecnm.mx>
2025-08-04 01:28:55 -0500

    Improve advantages section from landing page

--- src/modules/globals/components/Icons.tsx
+++ src/modules/globals/components/Icons.tsx
@@ -18,6 +18,27 @@ export function ArrowUp({ className = "", strokeWidth = "2" }: IconProps) {
 	);
 }
 
+export function ArrowsLeftRight({
+	className = "",
+	strokeWidth = "2",
+}: IconProps) {
+	return (
+		<svg
+			fill="none"
+			viewBox="0 0 24 24"
+			strokeWidth={strokeWidth}
+			stroke="currentColor"
+			className={className}
+		>
+			<path
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				d="M7.5 21 3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5"
+			/>
+		</svg>
+	);
+}
+
 export function Backward({ className = "", strokeWidth = "2" }: IconProps) {
 	return (
 		<svg
@@ -164,6 +185,24 @@ export function BoltSlash({ className = "", strokeWidth = "2" }: IconProps) {
 				strokeLinecap="round"
 				strokeLinejoin="round"
 				d="M11.412 15.655 9.75 21.75l3.745-4.012M9.257 13.5H3.75l2.659-2.849m2.048-2.194L14.25 2.25 12 10.5h8.25l-4.707 5.043M8.457 8.457 3 3m5.457 5.457 7.086 7.086m0 0L21 21"
+			/>
+		</svg>
+	);
+}
+
+export function CalendarDays({ className = "", strokeWidth = "2" }: IconProps) {
+	return (
+		<svg
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+			strokeWidth={strokeWidth}
+			className={className}
+		>
+			<path
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
 			/>
 		</svg>
 	);
@@ -380,6 +419,24 @@ export function InformationCircle({
 	);
 }
 
+export function Megaphone({ className = "", strokeWidth = "2" }: IconProps) {
+	return (
+		<svg
+			fill="none"
+			viewBox="0 0 24 24"
+			strokeWidth={strokeWidth}
+			stroke="currentColor"
+			className={className}
+		>
+			<path
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 1 1 0-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 0 1-1.44-4.282m3.102.069a18.03 18.03 0 0 1-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 0 1 8.835 2.535M10.34 6.66a23.847 23.847 0 0 0 8.835-2.535m0 0A23.74 23.74 0 0 0 18.795 3m.38 1.125a23.91 23.91 0 0 1 1.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 0 0 1.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 0 1 0 3.46"
+			/>
+		</svg>
+	);
+}
+
 export function Moon({ className = "", strokeWidth = "2" }: IconProps) {
 	return (
 		<svg
@@ -505,6 +562,24 @@ export function TrendingUp({ className = "", strokeWidth = "2" }: IconProps) {
 				strokeLinecap="round"
 				strokeLinejoin="round"
 				d="M2.25 18 9 11.25l4.306 4.306a11.95 11.95 0 0 1 5.814-5.518l2.74-1.22m0 0-5.94-2.281m5.94 2.28-2.28 5.941"
+			/>
+		</svg>
+	);
+}
+
+export function Trophy({ className = "", strokeWidth = "2" }: IconProps) {
+	return (
+		<svg
+			fill="none"
+			viewBox="0 0 24 24"
+			strokeWidth={strokeWidth}
+			stroke="currentColor"
+			className={className}
+		>
+			<path
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				d="M16.5 18.75h-9m9 0a3 3 0 0 1 3 3h-15a3 3 0 0 1 3-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 0 1-.982-3.172M9.497 14.25a7.454 7.454 0 0 0 .981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 0 0 7.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 0 0 2.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 0 1 2.916.52 6.003 6.003 0 0 1-5.395 4.972m0 0a6.726 6.726 0 0 1-2.749 1.35m0 0a6.772 6.772 0 0 1-3.044 0"
 			/>
 		</svg>
 	);
--- src/modules/landing/components/BentoGrid.tsx
+++ src/modules/landing/components/BentoGrid.tsx
@@ -1,19 +1,318 @@
+import {
+	ArrowsLeftRight,
+	BellAlert,
+	Bolt,
+	CalendarDays,
+	Clock,
+	Edit,
+	FaceHappy,
+	Forward,
+	Megaphone,
+	PresentationChartLine,
+	Scale,
+	TrendingUp,
+	Trophy,
+	WrenchScrewdriver,
+} from "@/modules/globals/components/Icons";
+import { Border } from "@/modules/globals/components/utils/Border";
+import Link from "next/link";
+import { DaysSelection } from "./bentogrid/DaysSelection";
+import { IconsAnimation } from "./bentogrid/IconsAnimation";
+import { IconTypes } from "@/modules/globals/types";
+
+function IconBubble({
+	className = "",
+	Icon,
+}: {
+	className?: string;
+	Icon: IconTypes;
+}) {
+	return (
+		<figure className={`border-1 p-1 w-fit rounded-full ${className}`}>
+			<Icon className="size-8" strokeWidth="1" />
+		</figure>
+	);
+}
+
 export function BentoGrid({ className = "" }: { className?: string }) {
 	return (
 		<section
-			className={`w-full max-w-5xl mx-auto my-16 flex flex-col gap-4 ${className}`}
+			className={`w-full max-w-5xl mx-auto my-16 flex flex-col gap-8 ${className}`}
 		>
-			<h2 className="text-2xl font-bold ld-main-fg">Explore Our Features</h2>
-			<main className="grid grid-cols-4 gap-6 *:rounded-lg *:min-h-32">
-				<article className="bg-zinc-900 row-span-2"></article>
-				<article className="bg-zinc-900 col-span-2 row-span-3"></article>
-				<article className="bg-zinc-900 row-span-2"></article>
-				<article className="bg-zinc-900 row-span-2"></article>
-				<article className="bg-zinc-900 row-span-2"></article>
-				<article className="bg-zinc-900 row-span-3"></article>
-				<article className="bg-zinc-900 row-span-3"></article>
-				<article className="bg-zinc-900 row-span-2"></article>
-				<article className="bg-zinc-900 row-span-2"></article>
+			<h2 className="text-4xl font-bold ld-main-fg">Explore Our Features</h2>
+			<main className="grid grid-cols-4 gap-6 *:relative *:rounded-lg *:min-h-32 *:shadow-2xl *:light:shadow-lg *:shadow-black/10 *:light:shadow-slate-400/20">
+				<article className="relative row-span-2 p-4 bg-radial-[200%_100%_at_120%_-10%]  from-accent/30 to-sec-background from-[-50%] to-50% light:to-light-sec-background">
+					<main className="relative z-3 flex flex-col gap-4 h-full">
+						<header className="font-medium ld-main-fg text-2xl max-w-45 flex-1 leading-[1]">
+							Start Your Journey Now
+						</header>
+						<footer className="text-base font-light leading-[1.2]">
+							<p className="">Totally for free</p>
+							<p className="ld-sec-fg text-sm">$0/month</p>
+						</footer>
+					</main>
+					<Border
+						className="bg-sec-foreground/30 mask-radial-[50%_50%] mask-radial-at-top-left mask-radial-from-0% mask-radial-to-150% mask-radial-from-black mask-radial-to-black/50 light:mask-none light:bg-foreground/10"
+						border={2}
+					/>
+				</article>
+
+				<article className="relative col-span-2 row-span-3 bg-radial-[200%_100%_at_20%_-10%]  from-accent/30 to-sec-background from-[-50%] to-50% light:to-light-sec-background">
+					<main className="relative z-3 grid place-content-center h-full">
+						<span className="tracking-tighter text-white text-8xl font-extrabold font-funnel-display">
+							<span className="ld-main-fg">Atlas</span>
+							<span className="text-primary">Way</span>
+						</span>
+					</main>
+					<Border
+						className="bg-sec-foreground/50 mask-radial-[50%_50%] mask-radial-at-top-left mask-radial-from-0% mask-radial-to-150% mask-radial-from-black mask-radial-to-black/30 light:mask-none light:bg-foreground/10"
+						border={2}
+					/>
+				</article>
+
+				<article className="relative bg-zinc-900 light:bg-accent row-span-2 flex flex-col gap-4 pt-3">
+					<main className="relative ld-main-fg text-2xl z-3 px-4 leading-[1] text-right">
+						Schedule Your Sessions
+					</main>
+					<footer className="relative flex-1 h-full rounded-b-lg overflow-hidden opacity-40">
+						<figure className="absolute left-[50%] translate-x-[-50%] w-fit mx-auto top-2 mask-radial-at-top mask-radial-from-0 mask-radial-to-60% mask-radial-from-black mask-radial-to-black/10">
+							<DaysSelection />
+						</figure>
+					</footer>
+					<Border
+						className="bg-sec-foreground/30 mask-radial-[50%_50%] mask-radial-at-top-left mask-radial-from-0% mask-radial-to-150% mask-radial-from-black mask-radial-to-black/50"
+						border={2}
+					/>
+				</article>
+
+				<article className="relative bg-zinc-900 row-span-2">
+					<main className="relative z-3 grid place-content-center h-full">
+						<header className="max-w-45 text-2xl font-bold text-accent text-center leading-[1.1]">
+							Create Your Own Routines
+						</header>
+					</main>
+					<Border
+						className="bg-sec-foreground/30 mask-radial-[50%_50%] mask-radial-at-top-left mask-radial-from-0% mask-radial-to-150% mask-radial-from-black mask-radial-to-black/50"
+						border={2}
+					/>
+				</article>
+
+				<article className="relative bg-gradient-to-br from-sec-background light:from-light-sec-background to-accent/30 from-50% to-150% row-span-2 p-4">
+					<main className="relative z-3">
+						<p className="text-xl text-main-foreground light:text-light-main-foreground">
+							Flexible routines to fit your lifestyle.
+						</p>
+					</main>
+					<div className="absolute z-0 inset-0 rounded-lg overflow-hidden">
+						<figure className="absolute -bottom-21 right-10 bg-primary rounded-full w-fit h-32 p-2">
+							<ArrowsLeftRight
+								className="size-6 text-main-foreground"
+								strokeWidth="1.5"
+							/>
+						</figure>
+					</div>
+					<Border
+						className="bg-sec-foreground/30 mask-radial-[50%_50%] mask-radial-at-top-left mask-radial-from-0% mask-radial-to-150% mask-radial-from-black mask-radial-to-black/50 light:mask-none light:bg-foreground/10"
+						border={2}
+					/>
+				</article>
+
+				<article className="relative ld-sec-bg row-span-3">
+					<main className="relative z-3 flex flex-col h-full p-4 overflow-hidden">
+						<header className="relative flex-1 grid place-content-center">
+							<figure className="relative z-1 bg-primary rounded-full p-1.5 shadow-lg shadow-primary/50">
+								<Megaphone
+									className="size-10 text-main-foreground mask-b-from-0% mask-b-to-100% mask-b-from-black mask-b-to-black/40"
+									strokeWidth="1"
+								/>
+							</figure>
+						</header>
+						<main className="text-center">
+							<header className="text-xl ld-main-fg font-bold">
+								Reminders
+							</header>
+							<p className="leading-[1.3] text-zinc-400">
+								Timely reminders for your sessions.
+							</p>
+						</main>
+						<div className="absolute inset-0 z-0 rounded-lg mask-radial-at-[50%_50%] mask-radial-from-0% mask-radial-to-80% opacity-20 light:opacity-100">
+							<div className="grid grid-cols-[repeat(5,2.4rem)] w-fit mx-auto opacity-0">
+								<span className="flex flex-col gap-0.5">
+									<IconBubble
+										Icon={Trophy}
+										className="border-accent text-accent"
+									/>
+									<IconBubble
+										Icon={Forward}
+										className="border-foreground/50 text-foreground/50"
+									/>
+								</span>
+								<span className="flex flex-col gap-0.5 translate-y-[-1.55rem]">
+									<IconBubble
+										Icon={FaceHappy}
+										className="border-foreground/50 text-foreground/50"
+									/>
+									<IconBubble
+										Icon={Clock}
+										className="border-foreground/50 text-foreground/50"
+									/>
+									<IconBubble
+										Icon={BellAlert}
+										className="border-accent text-accent"
+									/>
+								</span>
+								<span>
+									<IconBubble
+										Icon={Bolt}
+										className="border-accent text-accent"
+									/>
+								</span>
+								<span className="flex flex-col gap-0.5 translate-y-[-1.55rem]">
+									<IconBubble
+										Icon={Scale}
+										className="border-foreground/50 text-foreground/50"
+									/>
+									<IconBubble
+										Icon={WrenchScrewdriver}
+										className="border-accent text-accent"
+									/>
+									<IconBubble
+										Icon={PresentationChartLine}
+										className="border-foreground/50 text-foreground/50"
+									/>
+								</span>
+								<span className="flex flex-col gap-0.5">
+									<IconBubble
+										Icon={Edit}
+										className="border-foreground/50 text-foreground/50"
+									/>
+									<IconBubble
+										Icon={CalendarDays}
+										className="border-foreground/50 text-foreground/50"
+									/>
+								</span>
+							</div>
+						</div>
+					</main>
+
+					<Border
+						className="bg-sec-foreground/30 mask-radial-[50%_50%] mask-radial-at-top-left mask-radial-from-0% mask-radial-to-150% mask-radial-from-black mask-radial-to-black/50 light:mask-none light:bg-foreground/10"
+						border={2}
+					/>
+				</article>
+
+				<article className="ld-sec-bg row-span-3">
+					<main className="relative z-3 flex flex-col h-full overflow-hidden p-4">
+						<header className="relative flex-1 grid place-content-center">
+							<figure className="relative z-1 bg-primary rounded-full p-1.5 shadow-lg shadow-primary/50">
+								<TrendingUp
+									className="size-10 text-main-foreground mask-b-from-0% mask-b-to-100% mask-b-from-black mask-b-to-black/40"
+									strokeWidth="1"
+								/>
+							</figure>
+						</header>
+
+						<main className="relative z-1 text-center">
+							<header className="text-xl ld-main-fg font-bold">
+								Statistics
+							</header>
+							<p className="leading-[1.3] text-zinc-400">
+								Track your progress and performance.
+							</p>
+						</main>
+						<div className="absolute inset-0 z-0 rounded-lg mask-radial-at-[50%_50%] mask-radial-from-0% mask-radial-to-80% opacity-20 light:opacity-100">
+							<div className="grid grid-cols-[repeat(5,2.4rem)] w-fit mx-auto opacity-0">
+								<span className="flex flex-col gap-0.5">
+									<IconBubble
+										Icon={Clock}
+										className="border-foreground/50 text-foreground/50"
+									/>
+									<IconBubble
+										Icon={Scale}
+										className="border-accent text-accent"
+									/>
+								</span>
+								<span className="flex flex-col gap-0.5 translate-y-[-1.55rem]">
+									<IconBubble
+										Icon={Edit}
+										className="border-foreground/50 text-foreground/50"
+									/>
+									<IconBubble
+										Icon={BellAlert}
+										className="border-foreground/50 text-foreground/50"
+									/>
+									<IconBubble
+										Icon={FaceHappy}
+										className="border-foreground/50 text-foreground/50"
+									/>
+								</span>
+								<span>
+									<IconBubble
+										Icon={Bolt}
+										className="border-accent text-accent"
+									/>
+								</span>
+								<span className="flex flex-col gap-0.5 translate-y-[-1.55rem]">
+									<IconBubble
+										Icon={WrenchScrewdriver}
+										className="border-foreground/50 text-foreground/50"
+									/>
+									<IconBubble
+										Icon={Forward}
+										className="border-accent text-accent"
+									/>
+									<IconBubble
+										Icon={CalendarDays}
+										className="border-accent text-accent"
+									/>
+								</span>
+								<span className="flex flex-col gap-0.5">
+									<IconBubble
+										Icon={Trophy}
+										className="border-foreground/50 text-foreground/50"
+									/>
+									<IconBubble
+										Icon={PresentationChartLine}
+										className="border-foreground/50 text-foreground/50"
+									/>
+								</span>
+							</div>
+						</div>
+					</main>
+					<Border
+						className="bg-sec-foreground/30 mask-radial-[50%_50%] mask-radial-at-top-left mask-radial-from-0% mask-radial-to-150% mask-radial-from-black mask-radial-to-black/50 light:mask-none light:bg-foreground/10"
+						border={2}
+					/>
+				</article>
+
+				<article className="row-span-2 grid place-content-center bg-radial-[200%_200%_at_20%_110%]  from-accent/10 to-sec-background light:to-light-sec-background from-[-50%] to-50%">
+					<Link
+						href="/dashboard"
+						className="relative btn-lg rounded-full flex items-center gap-2 border-4 border-background/20 light:border-light-background outline-1 outline-sec-foreground/30 light:outline-foreground/20 overflow-hidden text-main-foreground"
+					>
+						<ArrowsLeftRight className="relative size-6 -ml-1 z-1" />
+						<span className="relative z-1 text-lg">Get Started</span>
+						<div className="absolute inset-0 bg-primary">
+							<span className="absolute bg-accent/50 inset-0 bottom-[50%] mask-radial-at-top mask-radial-from-0% mask-radial-to-75%"></span>
+						</div>
+					</Link>
+					<Border
+						className="bg-sec-foreground/30 mask-radial-[50%_50%] mask-radial-at-top-left mask-radial-from-0% mask-radial-to-150% mask-radial-from-black mask-radial-to-black/50 light:mask-none light:bg-foreground/10"
+						border={2}
+					/>
+				</article>
+
+				<article className="relative ld-sec-bg row-span-2">
+					<main className="flex flex-col items-center justify-center gap-3 h-full opacity-20 light:opacity-40">
+						<IconsAnimation direction="left" />
+						<IconsAnimation direction="right" />
+						<IconsAnimation direction="left" />
+					</main>
+					<Border
+						className="bg-sec-foreground/30 mask-radial-[50%_50%] mask-radial-at-top-left mask-radial-from-0% mask-radial-to-150% mask-radial-from-black mask-radial-to-black/50 light:mask-none light:bg-foreground/10"
+						border={2}
+					/>
+				</article>
 			</main>
 		</section>
 	);
