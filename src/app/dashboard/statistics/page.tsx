2e82f4e315cb1f5ae449bbaf727565473c7ef614
angelhdz-s <angelhdzsotelo15@gmail.com>
2025-08-06 21:29:57 -0500

    Fix statistics page error on using Routine component

--- src/app/dashboard/statistics/page.tsx
+++ src/app/dashboard/statistics/page.tsx
@@ -1,7 +1,6 @@
 import { PageContainer } from "@/modules/dashboard/components/page/PageContainer";
 import { PageContent } from "@/modules/dashboard/components/page/PageContent";
 import { PageHeader } from "@/modules/dashboard/components/page/PageHeader";
-import { Routine } from "@/modules/dashboard/components/routines/Routine";
 
 export default function StatisticsPage() {
 	return (
@@ -11,10 +10,7 @@ export default function StatisticsPage() {
 				className="flex items-center justify-between"
 			/>
 			<PageContent className="grid grid-cols-2 gap-4 w-fit">
-				<Routine />
-				<Routine />
-				<Routine />
-				<Routine />
+				Statistics Here
 			</PageContent>
 		</PageContainer>
 	);
