494c1c9f8b9f36a0cf49c79fb06ea313a967e6cc
angelhdz-s <angelhdzsotelo15@gmail.com>
2025-08-08 23:06:16 -0500

    Change First Routine and Session Components,

--- src/modules/dashboard/components/home/FirstRoutine.tsx
+++ src/modules/dashboard/components/home/FirstRoutine.tsx
@@ -1,7 +1,23 @@
+import Image from "next/image";
+import { ImageBackground } from "./ImageBackground";
+
 export function FirstRoutine({ className = "" }: { className?: string }) {
 	return (
-		<article className={`p-8 border-1 border-foreground/10 ${className}`}>
-			FirstRoutine
+		<article
+			className={`relative bg-background light:bg-light-sec-background back p-8 border-1 border-foreground/10 overflow-hidden ${className}`}
+		>
+			<h3 className="font-funnel-display ld-main-fg text-4xl">
+				Create Your Routine
+			</h3>
+			<ImageBackground>
+				<Image
+					src="/images/dashboard/routine-dark.png"
+					alt="Description of image"
+					width={762}
+					height={828}
+					className="aspect-[762/828]"
+				/>
+			</ImageBackground>
 		</article>
 	);
 }
--- src/modules/dashboard/components/home/FirstSession.tsx
+++ src/modules/dashboard/components/home/FirstSession.tsx
@@ -1,7 +1,23 @@
+import Image from "next/image";
+import { ImageBackground } from "./ImageBackground";
+
 export function FirstSession({ className = "" }: { className?: string }) {
 	return (
-		<article className={`p-8 border-1 border-foreground/10 ${className}`}>
-			FirstSession
+		<article
+			className={`relative bg-background light:bg-light-sec-background p-8 border-1 border-foreground/10 overflow-hidden ${className}`}
+		>
+			<h3 className="font-funnel-display ld-main-fg text-4xl">
+				Create Sessions For Your Routine
+			</h3>
+			<ImageBackground>
+				<Image
+					src="/images/dashboard/session-dark.png"
+					alt="Description of image"
+					width={773}
+					height={876}
+					className="aspect-[773/876]"
+				/>
+			</ImageBackground>
 		</article>
 	);
 }
