c7bddbd4601f0a50d36b0b104aa0be2705c8e527
angelhdz-s <angelhdzsotelo15@gmail.com>
2025-08-08 23:02:59 -0500

    Add description prop to PageHeader component from dashboard pages

--- src/modules/dashboard/components/page/PageHeader.tsx
+++ src/modules/dashboard/components/page/PageHeader.tsx
@@ -1,18 +1,27 @@
 export function PageHeader({
 	className = "",
+	description,
 	children,
 	title,
 }: {
 	className?: string;
 	children?: React.ReactNode;
+	description?: string;
 	title: string;
 }) {
 	return (
-		<header className={`ld-main-fg h-18 mb-8 flex items-center ${className}`}>
-			<h1 className="ld-main-fg-gradient text-5xl p-1 leading-[1] font-light tracking-tighter font-funnel-display">
-				{title}
-			</h1>
-			{children}
+		<header className={`ld-main-fg h-18 mb-8 ${className}`}>
+			<main className="flex items-center">
+				<h1 className="ld-main-fg-gradient text-5xl leading-[1] font-light tracking-tighter font-funnel-display">
+					{title}
+				</h1>
+				{children}
+			</main>
+			{description && (
+				<footer>
+					<p className="mt-2 text-lg text-foreground/70">{description}</p>
+				</footer>
+			)}
 		</header>
 	);
 }
