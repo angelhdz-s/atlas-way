2fac76321b09c68e5173033760839c14c637dcc8
angelhdz-s <angelhdzsotelo15@gmail.com>
2025-08-08 23:06:58 -0500

    Add some solid icons

--- src/modules/globals/components/Icons.tsx
+++ src/modules/globals/components/Icons.tsx
@@ -841,3 +841,37 @@ export function XMark({
 		</svg>
 	);
 }
+
+export function SolidCheckCircle({ className = "" }: { className?: string }) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 24 24"
+			fill="currentColor"
+			className={className}
+		>
+			<path
+				fillRule="evenodd"
+				d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+				clipRule="evenodd"
+			/>
+		</svg>
+	);
+}
+
+export function MiniCheckCircle({ className = "" }: { className?: string }) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 24 24"
+			fill="currentColor"
+			className={className}
+		>
+			<path
+				fillRule="evenodd"
+				d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm3.857-9.809a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z"
+				clipRule="evenodd"
+			/>
+		</svg>
+	);
+}
