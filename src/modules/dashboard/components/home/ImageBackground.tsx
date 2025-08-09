378de65cc25585c77d40394990e83cc0b5c92b3f
angelhdz-s <angelhdzsotelo15@gmail.com>
2025-08-08 23:05:43 -0500

    Add component ImageBackground for display example images as backgrounds of dashboard cards

---
+++ src/modules/dashboard/components/home/ImageBackground.tsx
@@ -0,0 +1,7 @@
+export function ImageBackground({ children }: { children: React.ReactNode }) {
+	return (
+		<div className="select-none absolute p-8 top-32 left-0 right-0 mx-auto w-[calc(100%-calc(var(--spacing)*16))] bg-full-black border rounded-lg border-foreground/20 mask-b-from-20%">
+			<main className="mask-b-from-0% mask-b-to-40%">{children}</main>
+		</div>
+	);
+}
