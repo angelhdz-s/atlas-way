55028c1927b8155edab4a6105f69d82e6b8461ed
angelhdz-s <angelhdzsotelo15@gmail.com>
2025-08-10 23:35:25 -0500

    Add Calendary and NotificationsList components for dashboard home page

--- src/modules/dashboard/components/home/Calendar.tsx
+++ src/modules/dashboard/components/home/Calendar.tsx
@@ -1,11 +1,129 @@
-import { CardTitle } from "./card/CardTitle";
+import {
+	Bolt,
+	BoltSlash,
+	CircleCheck,
+	Clock,
+} from "@/modules/globals/components/Icons";
+
+function CalendarHeader({ className = "" }: { className?: string }) {
+	return (
+		<header className={` ${className}`}>
+			<ul className="font-funnel-display font-light grid grid-cols-[1fr_auto_1fr]">
+				<li className="text-foreground/50">July</li>
+				<li className="text-xl ld-main-fg">August 2024</li>
+				<li className="text-foreground/50 text-right">September</li>
+			</ul>
+		</header>
+	);
+}
+
+function CalendarCurrentWeek({ className }: { className?: string }) {
+	return (
+		<ul
+			className={`flex items-end justify-around gap-2 text-sm leading-[1.2] *:rounded-lg *:w-full  *:px-3 *:py-1 ${className}`}
+		>
+			<li className="bg-subtle/[0.05] text-xs font-normal flex flex-col items-center gap-2 text-foreground/70">
+				<main>
+					<header>Thu</header>
+					<footer>07</footer>
+				</main>
+				<footer>
+					<CircleCheck className="size-5 mx-auto" />
+				</footer>
+			</li>
+			<li className="flex flex-col items-center gap-2 bg-subtle/10">
+				<main>
+					<header>Fri</header>
+					<footer>08</footer>
+				</main>
+				<footer>
+					<CircleCheck className="size-5 mx-auto" />
+				</footer>
+			</li>
+			<li className="flex flex-col items-center gap-2 leading-[1.2] bg-subtle/10 text-xl text-accent font-medium">
+				<main>
+					<header>Sat</header>
+					<footer>09</footer>
+				</main>
+				<footer>
+					<Clock className="size-5 mx-auto" />
+				</footer>
+			</li>
+			<li className="flex flex-col items-center gap-2 bg-subtle/10">
+				<main>
+					<header>Sun</header>
+					<footer>10</footer>
+				</main>
+				<footer>
+					<Bolt className="size-5 mx-auto" />
+				</footer>
+			</li>
+			<li className="bg-subtle/[0.05] text-xs font-normal flex flex-col items-center gap-2 text-foreground/70">
+				<main>
+					<header>Mon</header>
+					<footer>11</footer>
+				</main>
+				<footer>
+					<BoltSlash className="size-5 mx-auto" />
+				</footer>
+			</li>
+		</ul>
+	);
+}
+
+function CalendarNextWeek({ className }: { className?: string }) {
+	return (
+		<ul
+			className={`
+				flex items-end justify-around gap-2 text-xs text-foreground/50
+				*:rounded-lg *:bg-subtle/[0.05] *:w-full *:px-3 *:py-1
+				*:flex *:flex-col *:items-center *:gap-1 ${className}
+				`}
+		>
+			<li>
+				<main>Tue 12</main>
+				<footer>
+					<Bolt className="size-4 mx-auto" strokeWidth="1.5" />
+				</footer>
+			</li>
+			<li>
+				<main>Wed 13</main>
+				<footer>
+					<BoltSlash className="size-4 mx-auto" strokeWidth="1.5" />
+				</footer>
+			</li>
+			<li>
+				<main>Thu 14</main>
+				<footer>
+					<Bolt className="size-4 mx-auto" strokeWidth="1.5" />
+				</footer>
+			</li>
+			<li>
+				<main>Fri 15</main>
+				<footer>
+					<BoltSlash className="size-4 mx-auto" strokeWidth="1.5" />
+				</footer>
+			</li>
+			<li>
+				<main>Sat 16</main>
+				<footer>
+					<BoltSlash className="size-4 mx-auto" strokeWidth="1.5" />
+				</footer>
+			</li>
+		</ul>
+	);
+}
 
 export function Calendar({ className = "" }: { className?: string }) {
 	return (
 		<article
 			className={`col-span-4 flex flex-col gap-4 p-8 bg-background light:bg-light-sec-background border border-foreground/10 ${className}`}
 		>
-			<CardTitle title="Calendar" />
+			<CalendarHeader />
+			<main className="flex-1 leading-[1.2] flex flex-col justify-end gap-2 font-light text-center">
+				<CalendarCurrentWeek />
+				<CalendarNextWeek />
+			</main>
 		</article>
 	);
 }
--- src/modules/dashboard/components/home/NotificationsList.tsx
+++ src/modules/dashboard/components/home/NotificationsList.tsx
@@ -1,11 +1,70 @@
 import { CardTitle } from "./card/CardTitle";
 
+function NotificationListItem({
+	className = "",
+	notification,
+	date,
+	notseen,
+}: {
+	className?: string;
+	notification: string;
+	date: string;
+	notseen?: boolean;
+}) {
+	const notSeenClassName = notseen ? "font-medium bg-subtle/10 ld-main-fg" : "";
+	return (
+		<li
+			className={`flex items-center justify-between gap-2 py-2 px-3 outline-1 outline-subtle/10 ${notSeenClassName} ${className}`}
+		>
+			<main>{notification}</main>
+			<aside
+				className={`${notseen ? "text-foreground/80" : "text-foreground/50"}`}
+			>
+				{date}
+			</aside>
+		</li>
+	);
+}
+
+function Notifications({ className = "" }: { className?: string }) {
+	return (
+		<ul
+			className={`font-light flex rounded overflow-hidden flex-col gap-0 ${className}`}
+		>
+			<NotificationListItem
+				notification="Don't lose your progress!"
+				date="1 min"
+				notseen
+			/>
+			<NotificationListItem
+				notification="You have reached 120 sessions done"
+				date="5 mins"
+				notseen
+			/>
+			<NotificationListItem
+				notification="New follower: Jane Doe"
+				date="10 mins"
+				notseen
+			/>
+			<NotificationListItem
+				notification="Your routine has been updated"
+				date="30 mins"
+			/>
+		</ul>
+	);
+}
+
 export function NotificationsList({ className = "" }: { className?: string }) {
 	return (
 		<article
 			className={`flex flex-col gap-4 p-8 bg-background light:bg-light-sec-background border border-foreground/10 col-span-3 ${className}`}
 		>
-			<CardTitle title="Notifications" />
+			<header>
+				<CardTitle title="Notifications" />
+			</header>
+			<main className="text-sm">
+				<Notifications />
+			</main>
 		</article>
 	);
 }
