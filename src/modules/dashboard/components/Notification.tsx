d2847bf065abd8cd9aaec899d3699be9fee3b52e
angelhdz-s <angelhdzsotelo15@gmail.com>
2025-08-11 01:10:44 -0500

    Prepare Notification component for dynamic data

--- src/modules/dashboard/components/Notification.tsx
+++ src/modules/dashboard/components/Notification.tsx
@@ -1,37 +1,54 @@
+import { Trash } from "@/modules/globals/components/Icons";
+
 export function Notification({
 	className = "",
 	title,
 	description,
+	date,
+	notSeen = false,
 }: {
 	className?: string;
 	title: string;
 	description: string;
+	date: string;
+	notSeen?: boolean;
 }) {
 	return (
 		<article
-			className={`bg-zinc-900/50 px-4 py-2 border-1 border-zinc-900 rounded ${className}`}
+			className={`cursor-pointer px-4 py-1 border-1 border-foreground/10 hover:border-foreground/50 rounded ${notSeen ? "ld-sec-bg" : ""} ${className}`}
 		>
-			<main className="grid grid-cols-[auto_1fr_auto] gap-4 items-start">
+			<main className="flex items-center gap-4">
 				<div className="grid place-content-center my-auto">
 					<input type="checkbox" className="size-5" />
 				</div>
-				<main>
-					<h3>
-						{title}{" "}
-						<span className="text-foreground/50 text-sm ml-2">
-							3 minutes ago
-						</span>
-					</h3>
-					<p className="text-sm text-gray-500">{description}</p>
+				<main className="flex-1 flex flex-col gap-0">
+					<header className="flex items-center gap-4">
+						<h3
+							className={`text-base ${notSeen ? "ld-main-fg font-bold" : ""}`}
+						>
+							{title}
+						</h3>
+						{notSeen && (
+							<span className="block size-2.5 rounded-full bg-blue-500"></span>
+						)}
+					</header>
+					<footer>
+						<p className={`text-sm ${notSeen ? "" : "text-foreground/60"}`}>
+							{description}
+						</p>
+					</footer>
 				</main>
-				<aside className="flex items-center gap-2 justify-end my-auto *:cursor-pointer *:px-4 *:py-2 *:border-1 *:border-zinc-900 *:rounded-xl">
-					<button type="button" className="text-sm text-blue-500">
-						Check
-					</button>
-					<button type="button" className="text-sm text-red-600">
-						Delete
-					</button>
+				<aside className="w-12 text-center text-foreground/50 text-sm">
+					{date}
 				</aside>
+				<div className="flex items-center gap-2">
+					<button
+						type="button"
+						className="bg-foreground/10 text-sm text-red-600 cursor-pointer p-2 rounded"
+					>
+						<Trash className="size-4" strokeWidth="2" />
+					</button>
+				</div>
 			</main>
 		</article>
 	);
