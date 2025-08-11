1fd7da9a21524c8a4d6ab959cecf0823b06e60e4
angelhdz-s <angelhdzsotelo15@gmail.com>
2025-08-10 23:34:07 -0500

    Prepare charts components

--- src/app/dashboard/statistics/page.tsx
+++ src/app/dashboard/statistics/page.tsx
@@ -1,6 +1,10 @@
 import { PageContainer } from "@/modules/dashboard/components/page/PageContainer";
 import { PageContent } from "@/modules/dashboard/components/page/PageContent";
 import { PageHeader } from "@/modules/dashboard/components/page/PageHeader";
+import {
+	BarCharts,
+	LineChart,
+} from "@/modules/dashboard/components/statistics/Charts";
 
 export default function StatisticsPage() {
 	return (
@@ -9,8 +13,9 @@ export default function StatisticsPage() {
 				title="Your Statistics"
 				className="flex items-center justify-between"
 			/>
-			<PageContent className="grid grid-cols-2 gap-4 w-fit">
-				Statistics Here
+			<PageContent className="">
+				<LineChart width={800} height={400} />
+				<BarCharts width={800} height={400} />
 			</PageContent>
 		</PageContainer>
 	);
