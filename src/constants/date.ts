c318aaa40a5918c9002af47bb2d44dd6c44773d8
angelhdz-s <angelhdzsotelo15@gmail.com>
2025-08-19 02:55:22 -0500

    Add month days constants

---
+++ src/constants/date.ts
@@ -0,0 +1,7 @@
+export const MONTH_DAYS = [
+	31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,
+] as const;
+
+export const MONTH_DAYS_LEAP = [
+	31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,
+] as const;
