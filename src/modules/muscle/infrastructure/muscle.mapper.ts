4456ff1a5baad688ca62c93a609bfb9af8aaed21
angelhdz-s <angelhdzsotelo15@gmail.com>
2025-12-08 23:48:37 -0600

    Add muscle mapper

---
+++ src/modules/muscle/infrastructure/muscle.mapper.ts
@@ -0,0 +1,35 @@
+import { Prisma, Muscles as PrismaMuscle } from '@/prisma/client';
+import { Muscle as DomainMuscle, Muscle, NewMuscle, UpdateMuscle } from '../domain/muscle.entity';
+import { MuscleProps } from '../domain/muscle.schema';
+
+export class MuscleMapper {
+	static toDomain(data: PrismaMuscle): DomainMuscle {
+		const muscle: MuscleProps = {
+			id: data.id,
+			name: data.name,
+			description: data.description,
+			createdAt: data.createdAt,
+			updatedAt: data.updatedAt,
+			userId: data.userId,
+			muscleGroupId: data.muscularGroupId,
+		};
+
+		return new Muscle(muscle);
+	}
+
+	static toPersistenceCreate(data: NewMuscle): Prisma.MusclesCreateManyInput {
+		return {
+			muscularGroupId: data.muscleGroupId,
+			userId: data.userId,
+			name: data.name,
+			description: data.description,
+		};
+	}
+	static toPersistenceUpdate(data: UpdateMuscle): Prisma.MusclesUpdateInput {
+		const muscle: Prisma.MusclesUpdateInput = {};
+
+		if (data.description) muscle.description = data.description;
+		if (data.name) muscle.name = data.name;
+		return muscle;
+	}
+}
