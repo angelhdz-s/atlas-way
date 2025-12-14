59110b58b8ac292db55112c6cbc7a190ac13eea5
angelhdz-s <angelhdzsotelo15@gmail.com>
2025-12-14 16:09:11 -0600

    add auth route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,6 @@
-import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
+import { authOptions } from '@/modules/auth/infrastructure/nextAuth/auth.options';
+import NextAuth, { User } from 'next-auth';
 
-export const authOptions = {
-	providers: [
-		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID as string,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-		}),
-	],
-};
+const handler = NextAuth(authOptions);
 
-export default NextAuth(authOptions);
+export { handler as GET, handler as POST };
