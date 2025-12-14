664449cf45a101a1100dc444643ef88f6fd41cd7
angelhdz-s <angelhdzsotelo15@gmail.com>
2025-12-14 16:11:40 -0600

    Add user icon

--- src/modules/globals/components/Icons.tsx
+++ src/modules/globals/components/Icons.tsx
@@ -592,6 +592,16 @@ export function Search({ className, strokeWidth = defaultIconStrokeWidth }: Icon
 	);
 }
 
+export function UserIcon({ className, strokeWidth = defaultIconStrokeWidth }: IconProps) {
+	return (
+		<MainSvgBody strokeWidth={strokeWidth} className={className}>
+			<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+			<path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+			<path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+		</MainSvgBody>
+	);
+}
+
 /* MARK: Solid Icons */
 
 export function SolidCircleCheck({ className = '' }: { className?: string }) {
