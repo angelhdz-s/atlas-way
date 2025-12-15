c50dc72f3a54773900496f127b94de0fec5937c9
angelhdz-s <angelhdzsotelo15@gmail.com>
2025-12-15 04:02:32 -0600

    Add next steps to use react hook form

---
+++ src/app/_actions/exercise.actions.ts
@@ -0,0 +1,4 @@
+// Use case con todos los repositorios de exercises necesarios en el constructor:
+// - exerciseRepo: IExerciseRepository
+// - initialStatsRepo: IInitialStatsRepository
+// constructor (private exerciseRepo: IExRepo, private initialStatsRepo: IInitRepo) {}
