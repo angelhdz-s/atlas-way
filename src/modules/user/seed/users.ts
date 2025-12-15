88f17428ec5f26e1bf8b7f65206ae986e5b23d92
angelhdz-s <angelhdzsotelo15@gmail.com>
2025-12-14 18:21:09 -0600

    define default system user in users seed and its uuid

--- src/modules/user/seed/users.ts
+++ src/modules/user/seed/users.ts
@@ -1,7 +1,6 @@
 import { Prisma } from '../../../prisma/client';
 
 export const SYSTEM_ID = 'fbd0fb0b-41c0-43d4-823c-c7f32c80819b';
-export const ADMIN_ID = '868742e7-ec39-4461-b98e-e73032bf4f91';
 export const ROLES: Prisma.RolesCreateManyInput[] = [
 	{
 		id: 'system',
@@ -22,11 +21,5 @@ export const USERS: Prisma.UsersCreateManyInput[] = [
 		email: 'atlasway@corporation.com',
 		name: 'System',
 		roleId: 'system',
-	},
-	{
-		id: ADMIN_ID,
-		email: 'admin@dev.com',
-		name: 'Angel Admin',
-		roleId: 'admin',
 	},
 ];
