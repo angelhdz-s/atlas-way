e53c966b5dc1c7e3d278d7b623dacd44c1a71f9d
angelhdz-s <angelhdzsotelo15@gmail.com>
2025-12-15 04:02:10 -0600

    update user actions

--- src/app/_actions/user.actions.ts
+++ src/app/_actions/user.actions.ts
@@ -1,9 +1,10 @@
 'use server';
 
 import { GetCurrentUserUseCase } from '@/modules/user/application/get-current-user.usecase';
+import { GetUserByEmailUseCase } from '@/modules/user/application/get-user-by-email.usecase';
 import { User } from '@/modules/user/domain/user.entity';
 import { UserPrismaRepository } from '@/modules/user/infrastructure/user.prisma.repository';
-import { ActionResponse } from '@/shared/contracts/actions.response';
+import { ActionResponse, ActionResponseProps } from '@/shared/contracts/actions.response';
 export async function getUser(): ActionResponse<User | null> {
 	const repo = new UserPrismaRepository();
 	const currentUser = new GetCurrentUserUseCase(repo);
@@ -17,11 +18,25 @@ export async function getUser(): ActionResponse<User | null> {
 	};
 }
 
-export async function getUserId() {
+export async function getUserIdByEmail(email: User['email']): ActionResponse<User['id'] | null> {
+	const response: ActionResponseProps<User['id'] | null> = {
+		success: false,
+		message: '',
+		data: null,
+	};
+
 	const repo = new UserPrismaRepository();
-	const currentUser = new GetCurrentUserUseCase(repo);
+	const usecase = new GetUserByEmailUseCase(repo);
+	const user = await usecase.execute(email);
 
-	const user = await currentUser.execute();
+	if (!user) {
+		response.message = 'User not found';
+		return response;
+	}
 
-	return user ? user.id : null;
+	return {
+		success: true,
+		message: 'User found successfully',
+		data: user.id,
+	};
 }
