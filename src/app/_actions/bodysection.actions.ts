13d51db1f02895fb516d3e996c49a202e38dd4ca
angelhdz-s <angelhdzsotelo15@gmail.com>
2025-12-15 02:20:57 -0600

    add bodysection server action to the new usecase added

--- src/app/_actions/bodysection.actions.ts
+++ src/app/_actions/bodysection.actions.ts
@@ -1,13 +1,32 @@
 'use server';
 
+import { GetAllBodySectionsWithMuscularGroupsUseCase } from '@/modules/bodysection/application/get-bodysections-w-musculargroups.usecase';
 import { GetAllBodySectionsUseCase } from '@/modules/bodysection/application/get-bodysections.usecase';
-import { BodySection } from '@/modules/bodysection/domain/bodysection.entity';
+import {
+	BodySection,
+	BodySectionWithMuscularGroups,
+} from '@/modules/bodysection/domain/bodysection.entity';
 import { BodySectionPrismaReporisoty } from '@/modules/bodysection/infrastructure/bodysection.prisma.repository';
 import { ActionResponse } from '@/shared/contracts/actions.response';
 
 export async function getBodySections(): ActionResponse<BodySection[]> {
 	const repo = new BodySectionPrismaReporisoty();
 	const usecase = new GetAllBodySectionsUseCase(repo);
+
+	const bodySections = await usecase.execute();
+
+	return {
+		success: true,
+		message: 'Body sections were obtained',
+		data: bodySections,
+	};
+}
+
+export async function getBodySectionsWithMuscularGroups(): ActionResponse<
+	BodySectionWithMuscularGroups[]
+> {
+	const repo = new BodySectionPrismaReporisoty();
+	const usecase = new GetAllBodySectionsWithMuscularGroupsUseCase(repo);
 
 	const bodySections = await usecase.execute();
 
