3059595a82d34cbdf1247326eab8d488de5a1b19
angelhdz-s <angelhdzsotelo15@gmail.com>
2025-12-19 04:09:06 -0600

    Add dependency injection containers

---
+++ src/di/containers.ts
@@ -0,0 +1,7 @@
+import { ExerciseContainer } from '@/modules/exercise/container/exercise.container';
+import { UserContainer } from '@/modules/user/container/user.container';
+
+export const Containers = {
+	Exercise: ExerciseContainer,
+	User: UserContainer,
+};
