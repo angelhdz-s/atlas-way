59180ff5d4ea5b573e0032def8ca6ace83060241
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-01-02 17:03:08 -0600

    Fix proxy problems

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,3 +1,9 @@
+import { NextResponse } from 'next/server';
+
 export { default } from 'next-auth/middleware';
+
+export function proxy() {
+	return NextResponse.next();
+}
 
 export const config = { matcher: ['/dashboard'] };
