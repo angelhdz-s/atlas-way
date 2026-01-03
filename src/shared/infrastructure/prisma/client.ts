87fbad9a39c9107b852385e6f3e2d1428c80c555
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-01-03 06:17:10 -0600

    Make singleton prisma client and pg pool

--- src/shared/infrastructure/prisma/client.ts
+++ src/shared/infrastructure/prisma/client.ts
@@ -1,8 +1,14 @@
 import { PrismaClient } from '@/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { pool } from './pg';
 
-const adapter = new PrismaPg({
-	connectionString: process.env.DATABASE_URL,
-});
+const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
 
-export const prisma = new PrismaClient({ adapter });
+export const prisma =
+	globalForPrisma.prisma ||
+	new PrismaClient({
+		adapter: new PrismaPg(pool),
+		log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+	});
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
---
+++ src/shared/infrastructure/prisma/pg.ts
@@ -0,0 +1,11 @@
+import { Pool } from 'pg';
+
+const globalForPg = global as unknown as { pool: Pool | undefined };
+
+export const pool =
+	globalForPg.pool ??
+	new Pool({
+		connectionString: process.env.DATABASE_URL,
+		max: 5,
+	});
+if (process.env.NODE_ENV !== 'production') globalForPg.pool = pool;
