f7a492c200b4135b29c063bf0b9ea29d302d87d8
angelhdz-s <angelhdzsotelo@gmail.com>
2026-01-12 02:55:43 -0600

    update session container

--- src/modules/session/session.container.ts
+++ src/modules/session/session.container.ts
@@ -5,13 +5,19 @@ import { GetSessionById } from '@/modules/session/application/usecases/get-sessi
 import { UpdateSessions } from '@/modules/session/application/usecases/update-session';
 import { SessionPrismaRepository } from '@/modules/session/infrastructure/prisma/session.prisma.repository';
 import { prisma } from '@/shared/infrastructure/prisma/client';
+import { SessionToExercisePrismaRepository } from './link/infrastructure/prisma/session-to-exercise.prisma.repository';
 
 export const makeSessionModule = () => {
 	const sessionRepo = new SessionPrismaRepository(prisma);
+	const sessionToExerciseRepo = new SessionToExercisePrismaRepository(prisma);
 	return {
 		GetAllSessionsUseCase: new GetAllSessions(sessionRepo),
 		GetSessionByIdUseCase: new GetSessionById(sessionRepo),
-		CreateSessionUseCase: new CreateSession(sessionRepo, IdGeneratorContainer),
+		CreateSessionUseCase: new CreateSession(
+			sessionRepo,
+			sessionToExerciseRepo,
+			IdGeneratorContainer
+		),
 		UpdateSessionUseCase: new UpdateSessions(sessionRepo),
 	};
 };
