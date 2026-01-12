066c23fcf1987ac180c59c90a9ce0cc906b3d550
angelhdz-s <angelhdzsotelo@gmail.com>
2026-01-12 02:53:31 -0600

    Add getting exercises dynamically in create session page

--- src/app/dashboard/@modal/sessions/create/page.tsx
+++ src/app/dashboard/@modal/sessions/create/page.tsx
@@ -1,8 +1,14 @@
+import { getAllUserExercises } from '@/modules/exercise/presentation/exercise.actions';
 import { SessionModalForm } from '@/modules/session/presentation/ui/components/SessionModalForm';
 import { SelectOption } from '@/presentation/modules/form/types';
 
 export default async function CreateSessionPage() {
-	const exercises: SelectOption[] = [];
+	const exercisesRequest = await getAllUserExercises({ includeMuscles: true });
+	const data = exercisesRequest.success ? exercisesRequest.data : [];
+	const exercises: SelectOption[] = data.map((exercise) => ({
+		label: exercise.name,
+		value: exercise.id,
+	}));
 
 	return <SessionModalForm title="New Session" exercises={exercises} />;
 }
