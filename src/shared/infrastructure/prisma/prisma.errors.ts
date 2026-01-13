d2ecf9e8ddcc6430d85e2db93354d1e539c1bc28
angelhdz-s <angelhdzsotelo@gmail.com>
2026-01-12 19:16:03 -0600

    fix/abstract class specification forgotten

--- src/shared/infrastructure/prisma/prisma.errors.ts
+++ src/shared/infrastructure/prisma/prisma.errors.ts
@@ -1,6 +1,6 @@
 import { TechnicalError } from '../errors/technical.errors';
 
-export abstract class PrismaError extends TechnicalError {
+export class PrismaError extends TechnicalError {
 	constructor(message: string | null = null) {
 		super('UNAVAILABLE_PRISMA_SERVICE');
 	}
