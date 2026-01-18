8c8a90bca0c1c00c55439264bfe8c43b38a48b6f
angelhdz-s <angelhdzsotelo@gmail.com>
2026-01-18 16:54:17 -0600

    Update error handler container file name

--- src/shared/infrastructure/errors/errorHandler.container.ts
+++ src/shared/infrastructure/errors/errorHandler.container.ts
@@ -2,7 +2,7 @@ import { NextAuthErrorHandler } from '../nextauth/errors/next-auth.error.handler
 import { PrismaErrorHandler } from '../prisma/errors/prisma.error.handler';
 import { ITechnicalErrorHandler } from './technicalErrorsHanlder.interface';
 
-export const technicalErrorsContainer: ITechnicalErrorHandler[] = [
+export const errorHandlersContainer: ITechnicalErrorHandler[] = [
 	new NextAuthErrorHandler(),
 	new PrismaErrorHandler(),
 ];
