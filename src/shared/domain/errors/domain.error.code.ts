cddc48262f9220f40d64073db23f6f08b70fe40e
angelhdz-s <angelhdzsotelo@gmail.com>
2026-01-20 19:41:10 -0600

    feat(errors): Add exercise to muscle forgotten error code

--- src/shared/domain/errors/domain.error.code.ts
+++ src/shared/domain/errors/domain.error.code.ts
@@ -1,5 +1,6 @@
 import { AuthErrorCode } from '@/modules/auth/domain/errors/auth.error.code';
 import { ExerciseErrorCode } from '@/modules/exercise/domain/errors/exercise.error.code';
+import { ExerciseToMuscleErrorCode } from '@/modules/exercise/link/muscle/domain/errors/exercise-to-muscle.error.code';
 import { ExerciseInitialStatsErrorCode } from '@/modules/exerciseinitialstats/domain/errors/exerciseinitialstats.error.code';
 import { MuscleErrorCode } from '@/modules/muscle/domain/errors/muscle.error.code';
 import { NotificationErrorCode } from '@/modules/notification/domain/errors/notification.error.code';
@@ -12,6 +13,7 @@ export type DomainErrorCode =
 	| AuthErrorCode
 	| ExerciseErrorCode
 	| ExerciseInitialStatsErrorCode
+	| ExerciseToMuscleErrorCode
 	| MuscleErrorCode
 	| NotificationErrorCode
 	| RoutineErrorCode
