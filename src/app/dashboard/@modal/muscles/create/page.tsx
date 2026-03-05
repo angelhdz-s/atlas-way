0e6929300cb8633564c1b4ce9f3e6c3316c51e19
angelhdz-s <angelhdzsotelo@gmail.com>
2026-03-05 14:05:03 -0600

    refactor: remove irrelevant form

--- src/app/dashboard/@modal/muscles/create/page.tsx
+++ src/app/dashboard/@modal/muscles/create/page.tsx
@@ -1,5 +1,3 @@
-import { RoutineModalForm } from '@/modules/routine/presentation/ui/components/RoutineModalForm';
-
 export default function CreateMusclePage() {
-  return <RoutineModalForm title="New Muscle" />;
+  return null;
 }
