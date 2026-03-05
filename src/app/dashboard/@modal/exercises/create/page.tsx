c67d221c211866d71f9ac667be4f6a116d24b0e6
angelhdz-s <angelhdzsotelo@gmail.com>
2026-03-05 14:00:27 -0600

    refactor: fetching all muscles in creating exercise form page

--- src/app/dashboard/@modal/exercises/create/page.tsx
+++ src/app/dashboard/@modal/exercises/create/page.tsx
@@ -3,18 +3,12 @@ import { getAllMuscles } from '@/modules/muscle/presentation/muscle.actions';
 import type { SelectOption } from '@/presentation/modules/form/types';
 
 export default async function CreateExercisePage() {
-  const title = 'New Exercise';
   const dbMuscles = await getAllMuscles();
-  if (!dbMuscles.success || !dbMuscles.data)
-    return <ModalExerciseForm title={title} muscles={[]} />;
-
-  const muscles: SelectOption[] = dbMuscles.data.map(
-    (muscle) => ({
-      label: muscle.name,
-      value: muscle.id.toString(),
-    })
-  );
-  return (
-    <ModalExerciseForm title={title} muscles={muscles} />
-  );
+  const muscles: SelectOption[] = dbMuscles.data
+    ? dbMuscles.data.map((muscle) => ({
+        label: muscle.name,
+        value: muscle.id.toString(),
+      }))
+    : [];
+  return <ModalExerciseForm muscles={muscles} />;
 }
