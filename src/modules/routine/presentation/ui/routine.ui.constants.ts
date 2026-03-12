1102745592f8b9677341550fc0aecdb3120e24e8
angelhdz-s <angelhdzsotelo@gmail.com>
2026-03-12 02:48:49 -0500

    feat (routine):  add default week cycle days constant

--- src/modules/routine/presentation/ui/routine.ui.constants.ts
+++ src/modules/routine/presentation/ui/routine.ui.constants.ts
@@ -11,3 +11,34 @@ export const routineFormConfig: RoutineFormConfig = {
     label: 'Week',
   },
 };
+
+export const DEFAULT_WEEK_CYCLE_DAYS_DATA = [
+  {
+    value: 'sunday',
+    label: 'Sun',
+  },
+  {
+    value: 'monday',
+    label: 'Mon',
+  },
+  {
+    value: 'tuesday',
+    label: 'Tue',
+  },
+  {
+    value: 'wednesday',
+    label: 'Wed',
+  },
+  {
+    value: 'thursday',
+    label: 'Tue',
+  },
+  {
+    value: 'friday',
+    label: 'Friday',
+  },
+  {
+    value: 'saturday',
+    label: 'Saturday',
+  },
+];
