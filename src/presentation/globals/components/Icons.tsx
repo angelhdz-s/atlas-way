1f77fd8af57c01cd4c6c23fcac16cccc3f517c71
angelhdz-s <angelhdzsotelo@gmail.com>
2026-03-12 02:47:08 -0500

    feat: add new ZZ icon

--- src/presentation/globals/components/Icons.tsx
+++ src/presentation/globals/components/Icons.tsx
@@ -714,3 +714,12 @@ export function IconGripVertical(props: IconProps) {
     </MainSvgBody>
   );
 }
+
+export function IconZZ(props: IconProps) {
+  return (
+    <MainSvgBody {...props}>
+      <path d="M4 12h6l-6 8h6" />
+      <path d="M14 4h6l-6 8h6" />
+    </MainSvgBody>
+  );
+}
