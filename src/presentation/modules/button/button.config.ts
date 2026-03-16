8d3d786bf78216fb6f5485e1940618d871b1eef8
angelhdz-s <angelhdzsotelo@gmail.com>
2026-03-16 02:37:48 -0500

    style: remove unnecesary button variant size xxs

--- src/presentation/modules/button/button.config.ts
+++ src/presentation/modules/button/button.config.ts
@@ -2,7 +2,7 @@ import { twMerge } from 'tailwind-merge';
 import { tv, type VariantProps } from 'tailwind-variants';
 
 export const buttonVariant = tv({
-  base: 'flex w-fit cursor-pointer items-center gap-2 rounded-(--rounding) py-0.5 whitespace-nowrap transition-colors [--rounding:8px] disabled:cursor-not-allowed disabled:opacity-50',
+  base: 'flex w-fit cursor-pointer items-center gap-2 rounded-(--rounding) py-0.5 whitespace-nowrap transition-colors [--rounding:10px] disabled:cursor-not-allowed disabled:opacity-50',
   variants: {
     color: {
       primary: 'bg-primary fg-strong-dark hover:bg-primary/90',
@@ -24,11 +24,10 @@ export const buttonVariant = tv({
       square: 'aspect-square',
     },
     size: {
-      xxs: 'h-5 px-1 text-xs [--rounding:5px]',
-      xs: 'h-8 px-4 text-sm [--rounding:6px]',
-      sm: 'h-10 px-6 text-sm [--rounding:8px]',
-      md: 'h-12 px-7 text-base',
-      lg: 'h-12 px-7 text-lg',
+      xs: 'h-8 px-4 text-sm [--rounding:8px]',
+      sm: 'h-9 px-5 text-sm [--rounding:8px]',
+      md: 'h-10 px-5 text-base',
+      lg: 'h-11 px-6 text-base',
     },
   },
   compoundVariants: [
