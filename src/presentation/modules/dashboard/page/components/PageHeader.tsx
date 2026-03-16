77cfdf085f8fcf3b504c4840c49c5a95b8efecb7
angelhdz-s <angelhdzsotelo@gmail.com>
2026-03-16 02:30:47 -0500

    refactor: style and refactor Page Header component

--- src/presentation/modules/dashboard/page/components/PageHeader.tsx
+++ src/presentation/modules/dashboard/page/components/PageHeader.tsx
@@ -10,18 +10,12 @@ export function PageHeader({
   title: string;
 }) {
   return (
-    <header className={`fg-strong min-h-20 w-full py-4 ${className}`}>
-      <main className="flex w-full items-center gap-2">
-        <h1 className="font-funnel-display flex-1 pb-1 text-4xl leading-none font-bold tracking-tight">
-          {title}
-        </h1>
-        <aside>{children}</aside>
-      </main>
-      {description && (
-        <footer>
-          <p className="text-default/70 text-lg font-light">{description}</p>
-        </footer>
-      )}
+    <header className={`fg-strong flex w-full items-center pt-4 ${className}`}>
+      <div className="w-full space-y-1">
+        <h1 className="font-funnel-display text-2xl leading-none font-bold">{title}</h1>
+        {description && <p className="text-default/70 text-base font-light">{description}</p>}
+      </div>
+      <aside>{children}</aside>
     </header>
   );
 }
