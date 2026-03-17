055a6d3ebbcebe8ef51f16926a780d13bddc8743
angelhdz-s <angelhdzsotelo@gmail.com>
2026-03-17 04:34:17 -0500

    refactor: simplify card sub components

--- src/presentation/modules/dashboard/card/components/CardMain.tsx
+++ src/presentation/modules/dashboard/card/components/CardMain.tsx
@@ -1,3 +1,10 @@
-export function CardMain({ children }: { children: React.ReactNode }) {
-  return <main className="flex flex-1 flex-col gap-6">{children}</main>;
+import { twMerge } from 'tailwind-merge';
+
+type Props = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+export function CardMain({ children, className }: Props) {
+  return <main className={twMerge('flex flex-1 flex-col gap-4', className)}>{children}</main>;
 }
