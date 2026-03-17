e94f13e1de0ddfd33421bcb9fcdd03eb8e20222b
angelhdz-s <angelhdzsotelo@gmail.com>
2026-03-17 18:02:22 -0500

    feat: add table sub components

--- src/presentation/globals/components/Icons.tsx
+++ src/presentation/globals/components/Icons.tsx
@@ -359,9 +359,9 @@ export function IconMoon(props: IconProps) {
 export function IconDots(props: IconProps) {
   return (
     <MainSvgBody {...props}>
-      <path d="M5 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
-      <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
-      <path d="M19 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+      <path d="M4 12a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+      <path d="M11 12a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+      <path d="M18 12a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
     </MainSvgBody>
   );
 }
