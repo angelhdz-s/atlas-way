6f5b2df860bb721493cae05e5a3c84e58189f346
angelhdz-s <angelhdzsotelo@gmail.com>
2026-03-17 04:35:46 -0500

    fix: add new card components props

--- src/modules/session/presentation/ui/components/Session.tsx
+++ src/modules/session/presentation/ui/components/Session.tsx
@@ -1,13 +1,12 @@
-import { IconBarbell } from '@/presentation/globals/components/Icons';
-import { CardTags } from '../../../../../presentation/modules/dashboard/card/components/CardTags';
 import { Card } from '@/presentation/modules/dashboard/card/components/Card';
 import { CardHeader } from '@/presentation/modules/dashboard/card/components/CardHeader';
-import { CardSubHeader } from '@/presentation/modules/dashboard/card/components/CardSubHeader';
 import { CardMain } from '@/presentation/modules/dashboard/card/components/CardMain';
-import { CardFooter } from '@/presentation/modules/dashboard/card/components/CardFooter';
-import { CardButton } from '@/presentation/modules/dashboard/card/components/CardButton';
+import { CardSubHeader } from '@/presentation/modules/dashboard/card/components/CardSubHeader';
+import { CardTag } from '../../../../../presentation/modules/dashboard/card/components/CardTag';
+import { CardTitle } from '@/presentation/modules/dashboard/card/components/CardTitle';
 import { SessionExercisesList } from './SessionExerciseList';
 import type { SessionDTO } from '@/modules/session/application/dtos/session.dto';
+import { CardTagsWrapperFade } from '@/presentation/modules/dashboard/card/components/CardTagsWrapperFade';
 
 export function Session({ session }: { session: SessionDTO }) {
   const { name, description, exercises } = session;
@@ -23,31 +22,19 @@ export function Session({ session }: { session: SessionDTO }) {
 
   return (
     <Card type="dashboard" width="lg">
-      <CardHeader
-        title={name}
-        decoration={<span className="bg-unread block aspect-square size-4 rounded-full"></span>}
-      >
-        <CardSubHeader
-          counters={['1 hour ago', `${exercises.length} exercises`, '0 routines']}
-          description={description ?? ''}
-        />
+      <CardHeader>
+        <CardTitle title={name} className="" />
+        <CardSubHeader description={description ?? ''} />
       </CardHeader>
 
       <CardMain>
-        <CardTags
-          values={muscularGroups.map((m) => ({
-            selected: false,
-            value: m,
-          }))}
-        />
+        <CardTagsWrapperFade>
+          {muscularGroups.map((m) => (
+            <CardTag key={m} tag={{ value: m, selected: false }} />
+          ))}
+        </CardTagsWrapperFade>
         <SessionExercisesList exercises={sortedExercises} />
       </CardMain>
-      <CardFooter>
-        <CardButton>
-          <IconBarbell className="size-6" strokeWidth="1.3" />
-          Edit
-        </CardButton>
-      </CardFooter>
     </Card>
   );
 }
