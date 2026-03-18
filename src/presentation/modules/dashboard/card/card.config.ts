41514eee6b161a83aedfafdb1ba8a99f0094d660
angelhdz-s <angelhdzsotelo@gmail.com>
2026-03-17 19:52:48 -0500

    style: improve card component variant styles

--- src/presentation/modules/dashboard/card/card.config.ts
+++ src/presentation/modules/dashboard/card/card.config.ts
@@ -1,20 +1,24 @@
+import { twMerge } from 'tailwind-merge';
 import { tv, type VariantProps } from 'tailwind-variants';
 
 export const cardVariant = tv({
-  base: 'rounded-2xl border p-4',
+  base: twMerge(
+    'relative rounded-(--rounded) border border-transparent p-4 [--rounded:1rem]',
+    'before:absolute before:inset-px before:-z-1 before:rounded-[calc(var(--rounded)-1px)]',
+    'after:border-bd-strong after:pointer-events-none after:absolute after:inset-0 after:z-0 after:rounded-(--rounded) after:border',
+    'light:after:mask-linear-340 after:mask-linear-160 after:mask-linear-from-black after:mask-linear-from-0% after:mask-linear-to-black/30 after:mask-linear-to-50%'
+  ),
   variants: {
     border: {
-      simple: 'bg-middle border-bd-muted',
-      default: 'border-bd-muted light:border-white/50 relative',
-      highlighted: 'group relative border-transparent',
+      highlighted: 'group',
     },
     type: {
-      dashboard: 'flex flex-col gap-6 p-8 tracking-tight shadow-xl shadow-black/5',
+      dashboard: 'flex flex-col gap-6 p-8 tracking-tight shadow-xl shadow-black/2',
     },
     color: {
-      default: 'bg-middle',
-      disabled: 'bg-middle border-bd-default opacity-50',
-      main: 'from-secondary to-primary fg-strong-dark border-secondary before:from-accent bg-linear-45 bg-radial-[100%_100%_at_25%_25%] before:absolute before:inset-0 before:rounded-xl before:bg-radial-[200%_150%_at_200%_0%] before:to-transparent',
+      default: 'before:bg-middle',
+      disabled: 'before:bg-middle border-bd-default opacity-50',
+      main: 'from-secondary to-primary fg-strong-dark before:from-accent bg-linear-45 bg-radial-[100%_100%_at_25%_25%] before:absolute before:inset-0 before:rounded-xl before:bg-radial-[200%_150%_at_200%_0%] before:to-transparent after:border-transparent',
       special: 'bg-accent fg-strong-dark border-accent',
     },
     width: {
@@ -24,22 +28,8 @@ export const cardVariant = tv({
       lg: 'w-92',
     },
   },
-  compoundVariants: [
-    {
-      border: 'highlighted',
-      color: 'default',
-      class:
-        'before:border-bd-muted light:before:border-middle before:bg-middle bg-transparent before:absolute before:inset-0 before:-z-1 before:rounded-2xl before:border',
-    },
-    {
-      border: 'default',
-      color: 'default',
-      class:
-        'light:before:bg-white before:bg-bd-strong before:absolute before:-inset-0.5 before:-z-1 before:rounded-[17px] before:from-black before:to-black/50 before:mask-radial-[50%_50%] before:mask-radial-from-0 before:mask-radial-to-100% before:mask-radial-at-top-left',
-    },
-  ],
+
   defaultVariants: {
-    border: 'default',
     color: 'default',
   },
 });
