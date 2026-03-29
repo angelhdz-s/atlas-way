b30c3660363287b5ce854291f15ffa37ab9e3c65
angelhdz-s <angelhdzsotelo@gmail.com>
2026-03-29 18:38:32 -0500

    feat(exercise): add user's exercise ownership validation in delete exercise usecase

--- src/modules/exercise/application/use-cases/delete-exercise.ts
+++ src/modules/exercise/application/use-cases/delete-exercise.ts
@@ -1,18 +1,18 @@
+import { ExerciseNotFoundError, ExerciseOwnershipError } from '../../domain/errors/exercise-errors';
+import { Failure } from '@/shared/domain/result';
+import type { ExerciseProps } from '../../domain/exercise.types';
 import type { IExerciseRepository } from '@/modules/exercise/domain/exercise.repository';
 import type { UseCase } from '@/shared/application/use-case';
-import type { ExerciseProps } from '../../domain/exercise.types';
-import { Failure } from '@/shared/domain/result';
-import { ExerciseNotFoundError } from '../../domain/errors/exercise-errors';
+import type { UserProps } from '@/modules/user/domain/user.types';
 
 export class DeleteExercise implements UseCase {
   constructor(private exerciseRepository: IExerciseRepository) {}
 
-  async execute(exerciseId: ExerciseProps['id']) {
+  async execute(exerciseId: ExerciseProps['id'], userId: UserProps['id']) {
     const exerciseResult = await this.exerciseRepository.findById(exerciseId);
     if (!exerciseResult.success) return exerciseResult;
-
-    if (exerciseResult.data !== null) return Failure(new ExerciseNotFoundError());
-
+    if (exerciseResult.data === null) return Failure(new ExerciseNotFoundError());
+    if (exerciseResult.data.userId !== userId) return Failure(new ExerciseOwnershipError());
     return await this.exerciseRepository.delete(exerciseId);
   }
 }
