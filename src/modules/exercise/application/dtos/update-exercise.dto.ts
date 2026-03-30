8bf4a5ab2c8aabf157f4d356f8bbf7071dc3488f
angelhdz-s <angelhdzsotelo@gmail.com>
2026-03-30 03:41:22 -0500

    fix (exercise): make muscleIds prop optional in update exercise input dto type

--- src/modules/exercise/application/dtos/update-exercise.dto.ts
+++ src/modules/exercise/application/dtos/update-exercise.dto.ts
@@ -4,5 +4,5 @@ import type { UpdateDto } from '@/shared/application/dtos/create.types';
 
 type UpdateProps = Pick<ExerciseProps, 'name' | 'description' | 'sets' | 'reps' | 'weight'>;
 export type UpdateExerciseInput = UpdateDto<UpdateProps> & {
-  muscleIds: MuscleProps['id'][];
+  muscleIds?: MuscleProps['id'][];
 };
