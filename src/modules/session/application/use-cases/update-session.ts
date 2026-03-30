4597928bd2f6638175fd106893e315a5f911130a
angelhdz-s <angelhdzsotelo@gmail.com>
2026-03-30 03:55:34 -0500

    fix (session): add missing exerciseIds updating in update session use case

--- src/modules/session/application/use-cases/update-session.ts
+++ src/modules/session/application/use-cases/update-session.ts
@@ -1,15 +1,19 @@
-import type { UseCase } from '@/shared/application/use-case';
+import { Failure } from '@/shared/domain/result';
+import { SessionNotFoundError } from '../../domain/errors/session.errors';
+import type { IExerciseRepository } from '@/modules/exercise/domain/exercise.repository';
 import type { ISessionRepository } from '../../domain/session.repository';
 import type { SessionProps } from '../../domain/session.types';
 import type { UpdateSessionInput } from '../dtos/update-session.dto';
-import { Failure } from '@/shared/domain/result';
-import { SessionNotFoundError } from '../../domain/errors/session.errors';
+import type { UseCase } from '@/shared/application/use-case';
 
 export class UpdateSessions implements UseCase {
-  constructor(private repository: ISessionRepository) {}
+  constructor(
+    private sessionRepository: ISessionRepository,
+    private exerciseRepository: IExerciseRepository
+  ) {}
 
   async execute(id: SessionProps['id'], data: UpdateSessionInput) {
-    const sessionResult = await this.repository.findById(id);
+    const sessionResult = await this.sessionRepository.findById(id);
 
     if (!sessionResult.success || !sessionResult.data) {
       if (!sessionResult.success) return Failure(sessionResult.error);
@@ -20,7 +24,12 @@ export class UpdateSessions implements UseCase {
 
     if (data.name) session.changeName(data.name);
     if (data.description) session.changeDescription(data.description);
+    if (data.exerciseIds) {
+      const exercisesResult = await this.exerciseRepository.findByIds(data.exerciseIds);
+      if (!exercisesResult.success) return exercisesResult;
+      session.changeExercises(exercisesResult.data);
+    }
 
-    return await this.repository.update(session);
+    return await this.sessionRepository.update(session);
   }
 }
