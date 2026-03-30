343a6dbe1615e010a72b8c3823b9867469f441a3
angelhdz-s <angelhdzsotelo@gmail.com>
2026-03-30 03:02:42 -0500

    fix (exercise): add update exercise input missing props

--- src/modules/exercise/application/dtos/update-exercise.dto.ts
+++ src/modules/exercise/application/dtos/update-exercise.dto.ts
@@ -1,5 +1,5 @@
 import type { ExerciseProps } from '@/modules/exercise/domain/exercise.types';
 import type { UpdateDto } from '@/shared/application/dtos/create.types';
 
-type UpdateProps = Pick<ExerciseProps, 'name' | 'description'>;
+type UpdateProps = Pick<ExerciseProps, 'name' | 'description' | 'sets' | 'reps' | 'weight'>;
 export type UpdateExerciseInput = UpdateDto<UpdateProps>;
