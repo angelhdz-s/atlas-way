70cf33263a92e9b801d00637e25e17713b0d07fe
angelhdz-s <angelhdzsotelo@gmail.com>
2026-03-30 03:49:22 -0500

    fix (session): add missing exerciseIds property in upda session input dto type

--- src/modules/session/application/dtos/update-session.dto.ts
+++ src/modules/session/application/dtos/update-session.dto.ts
@@ -1,6 +1,9 @@
-import type { UpdateDto } from '@/shared/application/dtos/create.types';
+import type { ExerciseProps } from '@/modules/exercise/domain/exercise.types';
 import type { SessionProps } from '../../domain/session.types';
+import type { UpdateDto } from '@/shared/application/dtos/create.types';
 
 type UpdateProps = Pick<SessionProps, 'name' | 'description'>;
 
-export type UpdateSessionInput = UpdateDto<UpdateProps>;
+export type UpdateSessionInput = UpdateDto<UpdateProps> & {
+  exerciseIds?: ExerciseProps['id'][];
+};
