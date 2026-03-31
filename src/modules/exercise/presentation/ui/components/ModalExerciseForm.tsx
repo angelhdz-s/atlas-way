00c022ab6962dd089885ac95caaac0910c12426f
angelhdz-s <angelhdzsotelo@gmail.com>
2026-03-31 08:40:51 -0500

    feat: add received data from exercise form and distribute to corresponding field components

--- src/modules/exercise/presentation/ui/components/ModalExerciseForm.tsx
+++ src/modules/exercise/presentation/ui/components/ModalExerciseForm.tsx
@@ -1,20 +1,28 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-import { ModalForm } from '@/presentation/modules/form/components/modal-form/ModalForm';
-import { ModalFormButtons } from '@/presentation/modules/form/components/modal-form/ModalFormButtons';
-import type { SelectOption } from '@/presentation/modules/form/types';
-import { FormFieldSection } from '@/presentation/modules/form/components/FormFieldSection';
-import { IconBarbell } from '@/presentation/globals/components/Icons';
+import { createExerciseAction } from '../../exercise.actions';
+import { ExerciseDescriptionField } from './fields/ExerciseDescriptionField';
 import { ExerciseFormSchema } from '../schemas/exercise.schema';
 import { ExerciseMusclesField } from './fields/ExerciseMusclesField';
-import { createExerciseAction } from '../../exercise.actions';
 import { ExerciseNameField } from './fields/ExerciseNameField';
-import { ExerciseDescriptionField } from './fields/ExerciseDescriptionField';
-import { ExerciseSetsField } from './fields/ExerciseSetsField';
 import { ExerciseRepsField } from './fields/ExerciseRepsField';
+import { ExerciseSetsField } from './fields/ExerciseSetsField';
 import { ExerciseWeightField } from './fields/ExerciseWeightField';
+import { FormFieldSection } from '@/presentation/modules/form/components/FormFieldSection';
+import { IconBarbell } from '@/presentation/globals/components/Icons';
+import { ModalForm } from '@/presentation/modules/form/components/modal-form/ModalForm';
+import { ModalFormButtons } from '@/presentation/modules/form/components/modal-form/ModalFormButtons';
+import { useRouter } from 'next/navigation';
+import type { ExerciseDTO } from '@/modules/exercise/application/dtos/exercise.dto';
 import type { SelectOption } from '@/presentation/modules/form/form.types';
+
+export function ModalExerciseForm({
+  muscles,
+  data,
+}: {
+  muscles: SelectOption[];
+  data?: ExerciseDTO;
+}) {
   const router = useRouter();
 
   const handleSuccess = () => {
@@ -31,18 +39,20 @@ import type { SelectOption } from '@/presentation/modules/form/form.types';
     onSuccess: handleSuccess,
   };
 
+  const muscleIds = data?.muscles.map((m) => m.id.toString()) ?? [];
+
   return (
     <ModalForm title="Build your exercise" config={config} onClose={handleClose}>
       <section className="space-y-4">
-        <ExerciseNameField />
-        <ExerciseDescriptionField />
+        <ExerciseNameField value={data?.name} />
+        <ExerciseDescriptionField value={data?.description} />
       </section>
       <FormFieldSection Icon={IconBarbell} title="Metrics" className="flex items-start gap-4">
-        <ExerciseSetsField />
-        <ExerciseRepsField />
-        <ExerciseWeightField />
+        <ExerciseSetsField value={data?.sets} />
+        <ExerciseRepsField value={data?.reps} />
+        <ExerciseWeightField value={data?.weight} />
       </FormFieldSection>
-      <ExerciseMusclesField items={muscles} />
+      <ExerciseMusclesField items={muscles} selectedItems={muscleIds} />
       <ModalFormButtons onClose={handleClose} />
     </ModalForm>
   );
