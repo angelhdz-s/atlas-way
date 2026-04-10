d11cd064663451cb5a31073f93d3fd0a0d9feed2
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-04-10 00:47:33 -0500

    refactor: remove unnecesary and unused components

--- src/presentation/globals/components/Anchor.tsx
+++
@@ -1,5 +0,0 @@
-export type AnchorElementProps = React.AnchorHTMLAttributes<HTMLAnchorElement>;
-
-export function Button({ children, ...props }: AnchorElementProps) {
-  return <a {...props}>{children}</a>;
-}
--- src/presentation/globals/components/Button.tsx
+++
@@ -1,5 +0,0 @@
-export type ButtonElementProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
-
-export function Button({ children, ...props }: ButtonElementProps) {
-  return <button {...props}>{children}</button>;
-}
