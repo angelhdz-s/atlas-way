a5dab3638e7e6139adcda0e6a4f980e25f9f2f7a
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-04-11 06:25:59 -0500

    refactor: improve solid icon wrapper component name

--- src/presentation/globals/components/icons/SolidIcons.tsx
+++ src/presentation/globals/components/icons/SolidIcons.tsx
@@ -1,6 +1,6 @@
 import type { IconProps } from '@/presentation/globals/presentation.types';
 
-function MainSolidSvgBody({
+export function SIconWrapper({
   children,
   className,
   ariaHidden = true,
@@ -23,16 +23,16 @@ function MainSolidSvgBody({
 
 export function IconSolidCircleCheck({ className = '' }: { className?: string }) {
   return (
-    <MainSolidSvgBody className={className}>
+    <SIconWrapper className={className}>
       <path d="M17 3.34a10 10 0 1 1 -14.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 14.995 -8.336zm-1.293 5.953a1 1 0 0 0 -1.32 -.083l-.094 .083l-3.293 3.292l-1.293 -1.292l-.094 -.083a1 1 0 0 0 -1.403 1.403l.083 .094l2 2l.094 .083a1 1 0 0 0 1.226 0l.094 -.083l4 -4l.083 -.094a1 1 0 0 0 -.083 -1.32z" />
-    </MainSolidSvgBody>
+    </SIconWrapper>
   );
 }
 
 export function IconSolidCircleX({ className = '' }: { className?: string }) {
   return (
-    <MainSolidSvgBody className={className}>
+    <SIconWrapper className={className}>
       <path d="M17 3.34a10 10 0 1 1 -14.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 14.995 -8.336zm-6.489 5.8a1 1 0 0 0 -1.218 1.567l1.292 1.293l-1.292 1.293l-.083 .094a1 1 0 0 0 1.497 1.32l1.293 -1.292l1.293 1.292l.094 .083a1 1 0 0 0 1.32 -1.497l-1.292 -1.293l1.292 -1.293l.083 -.094a1 1 0 0 0 -1.497 -1.32l-1.293 1.292l-1.293 -1.292l-.094 -.083z" />
-    </MainSolidSvgBody>
+    </SIconWrapper>
   );
 }
