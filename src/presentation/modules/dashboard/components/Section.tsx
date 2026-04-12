b56c1be7fb2a0ffc8c927e5901fd5e16f7305436
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-04-12 17:49:23 -0500

    refactor: update arbitrary values in tailwind classes

--- src/presentation/modules/dashboard/components/Section.tsx
+++ src/presentation/modules/dashboard/components/Section.tsx
@@ -15,7 +15,7 @@ export function Section({
         <header className="mb-2">
           <h3 className="fg-strong flex items-center gap-4 text-2xl font-semibold">
             <span className="block w-fit">{title}</span>
-            <span className="bg-front block h-[2px] w-full flex-1 mask-r-from-0 mask-r-to-150%"></span>
+            <span className="bg-front block h-0.5 w-full flex-1 mask-r-from-0 mask-r-to-150%"></span>
           </h3>
           {description && <p>{description}</p>}
         </header>
