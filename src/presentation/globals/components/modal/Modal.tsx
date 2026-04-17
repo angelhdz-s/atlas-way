b59556c0891d8531700f46ee60cb9c69449a8699
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-04-16 23:05:29 -0500

    feat: add new custom hook useLayer in Modal component

--- src/presentation/globals/components/modal/Modal.tsx
+++ src/presentation/globals/components/modal/Modal.tsx
@@ -1,6 +1,6 @@
 'use client';
-import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import { useLayer } from '@/presentation/globals/hooks/useLayer';
 export function Modal({
   className = '',
   children,
@@ -10,27 +10,21 @@ export function Modal({
 }) {
   const router = useRouter();
 
-  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+  const handleClose = () => {
     router.back();
-    e.stopPropagation();
   };
 
-  useEffect(() => {
-    const handleEscape = (e: KeyboardEvent) => {
-      if (e.key === 'Escape') {
-        e.stopPropagation();
-        router.back();
-      }
-    };
-
-    document.addEventListener('keydown', handleEscape);
-    return () => document.removeEventListener('keydown', handleEscape);
-  }, [router]);
+  const { ref } = useLayer({
+    onClose: handleClose,
+    isOpen: true,
+  });
 
   return (
     <div className={`fixed inset-0 z-50 m-auto flex items-center justify-center ${className}`}>
-      <div className="bg-middle z-50 rounded-2xl shadow-lg">{children}</div>
-      <div className="fixed inset-0 z-0 bg-black/30 select-none" onClick={handleClick}></div>
+      <div ref={ref} className="bg-middle z-50 rounded-2xl shadow-lg">
+        {children}
+      </div>
+      <div className="fixed inset-0 z-0 bg-black/30 select-none"></div>
     </div>
   );
 }
