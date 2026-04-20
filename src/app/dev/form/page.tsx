f5e7d02879c280395c0e415fd2497163bcdce8ed
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-04-20 03:36:18 -0500

    refactor(dev form): add three versions of form components with different backgrouds to see constrast

--- src/app/dev/form/page.tsx
+++ src/app/dev/form/page.tsx
@@ -1,20 +1,26 @@
-import { FormComponents } from './components/FormComponents';
+import { DevFormComponents } from './components/DevFormComponents';
 
-const Separator = ({ title, children }: { title: string; children: React.ReactNode }) => (
-  <div className="bg-middle space-y-4">
-    <h5 className="font-funnel-display fg-strong border-bd-muted rounded-xl border-b p-4 text-center text-4xl font-bold">
-      {title}
-    </h5>
-    <div className="p-8 pt-0">{children}</div>
+const Group = ({ title, children }: { title: string; children: React.ReactNode }) => (
+  <div className="w-100 space-y-4">
+    <h5 className="font-funnel-display fg-strong text-center text-4xl font-bold">{title}</h5>
+    <div>{children}</div>
   </div>
 );
 
 export default function FormPage() {
   return (
-    <div className="flex flex-wrap items-start justify-center gap-8">
-      <Separator title="Basic fields">
-        <FormComponents />
-      </Separator>
+    <div className="w-full space-y-8 overflow-x-auto">
+      <section className="flex items-center justify-center gap-4">
+        <Group title="Back">
+          <DevFormComponents className="outline-bd-default" />
+        </Group>
+        <Group title="Middle">
+          <DevFormComponents className="bg-middle outline-bd-default" />
+        </Group>
+        <Group title="Front">
+          <DevFormComponents className="bg-front outline-bd-default" />
+        </Group>
+      </section>
     </div>
   );
 }
