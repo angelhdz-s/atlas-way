24cd2db31548bae287a577c940007510501b1de4
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-04-20 02:11:59 -0500

    refactor(dev form): remove unnecesary environment control in dev form debug page

--- src/app/dev/form/page.tsx
+++ src/app/dev/form/page.tsx
@@ -1,7 +1,7 @@
 import { FormComponents } from './components/FormComponents';
 
 const Separator = ({ title, children }: { title: string; children: React.ReactNode }) => (
-  <div className="bg-front space-y-4">
+  <div className="bg-middle space-y-4">
     <h5 className="font-funnel-display fg-strong border-bd-muted rounded-xl border-b p-4 text-center text-4xl font-bold">
       {title}
     </h5>
@@ -10,10 +10,6 @@ const Separator = ({ title, children }: { title: string; children: React.ReactNo
 );
 
 export default function FormPage() {
-  if (process.env.NODE_ENV === 'production') {
-    return null;
-  }
-
   return (
     <div className="flex flex-wrap items-start justify-center gap-8">
       <Separator title="Basic fields">
