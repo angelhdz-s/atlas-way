ef6a51b74a08c7ebe574fe0ed67e85fdb343d1d8
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-04-19 21:21:41 -0500

    refactor(SelectBoxOption): improve accessibility and use twMerge handling classNames easier

--- src/presentation/modules/form/components/SelectBoxOption.tsx
+++ src/presentation/modules/form/components/SelectBoxOption.tsx
@@ -1,4 +1,5 @@
 import type { SelectOption } from '@/presentation/modules/form/form.types';
+import { twMerge } from 'tailwind-merge';
 
 export function SelectBoxOption({
   option,
@@ -13,11 +14,22 @@ export function SelectBoxOption({
     ? 'border-complete fg-complete font-medium light:fg-green-700 light:border-green-600'
     : 'border-subtle/20';
   return (
-    <label
-      className={`bg-middle hover:border-complete hover:fg-complete light:hover:fg-green-900 w-fit cursor-pointer rounded-lg border px-3 py-1 ${isActiveClass}`}
-    >
-      <span className="text-sm">{option.label}</span>
-      <input className="hidden" type="checkbox" onChange={() => onSelect(option.value)} />
+    <label className="relative">
+      <input
+        type="checkbox"
+        checked={isSelected}
+        onChange={() => onSelect(option.value)}
+        className="peer absolute size-0 opacity-0"
+        readOnly
+      />
+      <div
+        className={twMerge(
+          'outline-focus bg-middle hover:border-complete hover:fg-complete light:hover:fg-green-900 w-fit cursor-pointer rounded-lg border px-3 py-1 text-sm peer-focus:outline-2',
+          isActiveClass
+        )}
+      >
+        {option.label}
+      </div>
     </label>
   );
 }
