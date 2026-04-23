af46e40dda132ce78dc51e376246f66ee1d66fe0
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-04-23 16:53:31 -0500

    feat(use-case Login): add session already active validation

--- src/modules/auth/application/use-cases/login.ts
+++ src/modules/auth/application/use-cases/login.ts
@@ -1,10 +1,11 @@
-import { Success } from '@/shared/domain/result';
+import { Failure, Success } from '@/shared/domain/result';
 import { User } from '@/modules/user/domain/user.entity';
 import type { UseCase } from '@/shared/application/use-case';
 import type { IAuthRepository } from '@/modules/auth/domain/auth.respository';
 import type { IUserRepository } from '@/modules/user/domain/user.repository';
 import type { LoginInput } from '@/modules/auth/application/dtos/login-input.dto';
 import type { IdGeneratorRepository } from '@/shared/application/id-generator';
+import { SessionAlreadyActive } from '../../domain/errors/auth.errors';
 
 export class Login implements UseCase {
   constructor(
@@ -13,6 +14,9 @@ export class Login implements UseCase {
     readonly generatorRepository: IdGeneratorRepository
   ) {}
   async execute(data: LoginInput) {
+    const sessionResult = await this.authRepository.getSession();
+    if (!sessionResult.success) return sessionResult;
+    if (sessionResult.data) return Failure(new SessionAlreadyActive());
     const userResult = await this.userRepository.findByEmail(data.email);
     if (!userResult.success) return userResult;
     if (userResult.data) return Success(null);
