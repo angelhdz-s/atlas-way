a69f4604d70713ec3e66bf1d6e7612b14a5ffadd
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-04-23 17:10:09 -0500

    feat(auth): add login and logout server actions

--- src/modules/auth/presentation/auth.actions.ts
+++ src/modules/auth/presentation/auth.actions.ts
@@ -1,6 +1,11 @@
 import { getContainer } from '@/di/containers';
-import type { ActionResponse } from '@/shared/presentation/action.response';
+import {
+  ActionFailure,
+  ActionSuccess,
+  type ActionResponse,
+} from '@/shared/presentation/action.response';
 import type { AuthSession } from '@/modules/auth/domain/errors/auth-session.types';
+import { nextAuthLogin } from '@/modules/auth/infrastructure/next-auth/auth.next-auth.config';
 
 export async function getSession(): ActionResponse<AuthSession | null> {
   const container = getContainer();
@@ -17,4 +22,22 @@ export async function getSession(): ActionResponse<AuthSession | null> {
     message: 'Session obtained successfully',
     data: sessionResult.data,
   };
+}
+
+export async function login(): ActionResponse<null> {
+  try {
+    await nextAuthLogin();
+    return ActionSuccess(null, 'Logged successfully');
+  } catch (e) {
+    console.error(e);
+    return ActionFailure('Error trying to log in');
+  }
+}
+
+export async function logout(): ActionResponse<null> {
+  const container = getContainer();
+  const logout = container.auth.LogoutUseCase;
+  const logoutResult = await logout.execute();
+  if (!logoutResult.success) return ActionFailure(logoutResult.error.message);
+  return ActionSuccess(null, 'Logged successfully');
 }
