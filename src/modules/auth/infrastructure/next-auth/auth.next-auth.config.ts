0c35682914b30aaa229181a3c6d0b63f58495cfa
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-04-23 16:51:15 -0500

    refactor(auth): improve next auth login and logout functions name for clarity

---
+++ src/modules/auth/infrastructure/next-auth/auth.next-auth.config.ts
@@ -0,0 +1,12 @@
+import { signIn as nextAuthSignIn, signOut as nextAuthSignOut } from 'next-auth/react';
+import { AUTH_PROVIDER } from '@/modules/auth/infrastructure/next-auth/auth.next-auth.constants';
+
+export const nextAuthLogin = () =>
+  nextAuthSignIn(AUTH_PROVIDER, {
+    redirect: false,
+  });
+
+export const nextAuthLogout = () =>
+  nextAuthSignOut({
+    redirect: false,
+  });
