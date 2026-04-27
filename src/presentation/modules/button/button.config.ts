a94c822c827189bacee9e7f0f61f106de65de1e8
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-04-27 01:50:40 -0500

    feat(Button): add "outline" color, disabled, and "text", "icon", "iconText" type variants

--- src/presentation/modules/button/button.config.ts
+++ src/presentation/modules/button/button.config.ts
@@ -7,14 +7,17 @@ export const buttonVariant = tv({
     color: {
       primary: 'bg-primary text-fg-strong-dark hover:bg-primary/90',
       subtle: 'bg-fill-top hover:text-fg-subtle',
+      outline: 'hover:text-fg-subtle hover:bg-fill-top ring-bd-strong ring-1',
       simple: twMerge(
         'hover:text-fg-subtle hover:bg-bd-muted/50 border border-transparent bg-transparent',
         'light:hover:bg-black/3'
       ),
     },
     type: {
-      normal: true,
+      text: true,
+      icon: 'aspect-square',
       square: 'aspect-square',
+      iconText: 'gap',
     },
     size: {
       xs: 'h-8 px-4 text-sm',
@@ -22,15 +25,22 @@ export const buttonVariant = tv({
       md: 'h-10 px-5 text-base',
       lg: 'h-11 px-6 text-base',
     },
+    disabled: {
+      true: 'cursor-not-allowed opacity-50',
+    },
   },
   compoundVariants: [
     {
       type: 'square',
       class: 'justify-center p-0',
     },
+    {
+      type: 'icon',
+      class: 'justify-center p-0',
+    },
   ],
   defaultVariants: {
-    type: 'normal',
+    type: 'text',
     size: 'md',
   },
 });
