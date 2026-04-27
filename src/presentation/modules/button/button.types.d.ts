c3454d5cd599e8a582c735f84b33619232e6f5e4
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-04-27 01:09:19 -0500

    feat(Button type): add conditional and restricted props type

--- src/presentation/modules/button/button.types.d.ts
+++ src/presentation/modules/button/button.types.d.ts
@@ -1,6 +1,55 @@
+import type { IconTypes } from '@/presentation/globals/presentation.types';
 import type { ButtonVariantProps } from '@/presentation/modules/button/button.config';
+import type { NonEmptyString } from '@/shared/shared.types';
 
-export type VariantButtonType<T> = {
-  variantConfig?: ButtonVariantProps;
-  className?: string;
-} & T;
+export type VariantButtonType<T> = T & PossibleButtonVariantProps;
+
+type PossibleButtonVariantProps =
+  | {
+      variant?: {
+        size?: ButtonVariantProps['size'];
+        size?: ButtonVariantProps['color'];
+        type?: undefined;
+      };
+      variantConfig?: ButtonVariantProps;
+      className?: string;
+      Icon?: never;
+      children: React.ReactNode;
+      'aria-label'?: NonEmptyString<string>;
+    }
+  | {
+      variant?: {
+        size?: ButtonVariantProps['size'];
+        size?: ButtonVariantProps['color'];
+        type?: 'icon';
+      };
+      variantConfig?: ButtonVariantProps;
+      className?: string;
+      Icon: IconTypes;
+      children?: never;
+      'aria-label': NonEmptyString<string>;
+    }
+  | {
+      variant?: {
+        size?: ButtonVariantProps['size'];
+        size?: ButtonVariantProps['color'];
+        type?: 'iconText';
+      };
+      variantConfig?: ButtonVariantProps;
+      className?: string;
+      Icon: IconTypes;
+      children: React.ReactNode;
+      'aria-label'?: NonEmptyString<string>;
+    }
+  | {
+      variant?: {
+        size?: ButtonVariantProps['size'];
+        size?: ButtonVariantProps['color'];
+        type?: 'text';
+      };
+      variantConfig?: ButtonVariantProps;
+      className?: string;
+      Icon?: never;
+      children: React.ReactNode;
+      'aria-label'?: NonEmptyString<string>;
+    };
