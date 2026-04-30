f6a0babc388901ff77125a6039fcc65ba2557e76
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-04-30 00:07:14 -0500

    refactor: remove unsued pages

--- src/app/dashboard/@modal/routines/create/sessions/page.tsx
+++
@@ -1,5 +0,0 @@
-import { RoutineModalForm } from '@/modules/routine/presentation/ui/components/RoutineModalForm';
-
-export default function CreateRoutineSessionsPage() {
-  return <RoutineModalForm title="New Routine" />;
-}
