4542e27f4e5902e53f4700f04a6fd9e9bb8876b2
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-05-02 00:57:54 -0500

    refactor(exercise server-actions): use Promise type directly in response type of async functions

--- src/modules/exercise/presentation/exercise.actions.ts
+++ src/modules/exercise/presentation/exercise.actions.ts
@@ -6,7 +6,7 @@ import { ExerciseFormSchema } from '@/modules/exercise/presentation/ui/schemas/e
 import { ExerciseMapper } from '@/modules/exercise/infrastructure/exercise.mapper';
 import { getContainer } from '@/di/containers';
 import { getCurrentUserId } from '@/modules/user/presentation/user.actions';
-import type { ActionResponse } from '@/shared/presentation/action.response';
+import type { ActionResponseProps } from '@/shared/presentation/action.response';
 import type { CreateExerciseInput } from '@/modules/exercise/application/dtos/create-exercise.dto';
 import type { ExerciseDTO } from '@/modules/exercise/application/dtos/exercise.dto';
 import type { ExerciseProps } from '@/modules/exercise/domain/exercise.types';
@@ -14,7 +14,7 @@ import type { ExerciseFormProps } from '@/modules/exercise/presentation/ui/schem
 
 export async function createExerciseAction(
   data: ExerciseFormProps
-): ActionResponse<ExerciseDTO | null> {
+): Promise<ActionResponseProps<ExerciseDTO | null>> {
   const exerciseParseResult = ExerciseFormSchema.safeParse(data);
 
   if (!exerciseParseResult.success) {
@@ -48,7 +48,7 @@ export async function createExerciseAction(
   return ActionSuccess(exerciseDTO, 'Exercise created successfully');
 }
 
-export async function getAllUserExercises(): ActionResponse<ExerciseDTO[]> {
+export async function getAllUserExercises(): Promise<ActionResponseProps<ExerciseDTO[]>> {
   const container = getContainer();
 
   const userIdResult = await getCurrentUserId();
@@ -66,7 +66,9 @@ export async function getAllUserExercises(): ActionResponse<ExerciseDTO[]> {
   return ActionSuccess(exercisesDTOs, 'User exercises found successfully');
 }
 
-export async function deleteExercise(exerciseId: ExerciseProps['id']): ActionResponse<ExerciseDTO> {
+export async function deleteExercise(
+  exerciseId: ExerciseProps['id']
+): Promise<ActionResponseProps<ExerciseDTO>> {
   const container = getContainer();
 
   const userIdResult = await getCurrentUserId();
