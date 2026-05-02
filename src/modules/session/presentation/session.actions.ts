1bfbdeddc0e02b15a71f5cfaf583ebf5a7171b15
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-05-02 04:11:08 -0500

    refactor(session): add Promise type directly in server action response

--- src/modules/session/presentation/session.actions.ts
+++ src/modules/session/presentation/session.actions.ts
@@ -6,15 +6,17 @@ import { getCurrentUserId } from '@/modules/user/presentation/user.actions';
 import { SessionMapper } from '@/modules/session/infrastructure/session.mapper';
 import {
   ActionFailure,
-  type ActionResponse,
   ActionSuccess,
+  type ActionResponseProps,
 } from '@/shared/presentation/action.response';
 import type { SessionForm } from '@/modules/session/presentation/ui/config/session.schema';
 import type { CreateSessionInput } from '@/modules/session/application/dtos/create-session.dto';
 import type { SessionDTO } from '@/modules/session/application/dtos/session.dto';
 import type { SessionProps } from '@/modules/session/domain/session.types';
 
-export async function createSessionAction(data: SessionForm): ActionResponse<SessionDTO> {
+export async function createSessionAction(
+  data: SessionForm
+): Promise<ActionResponseProps<SessionDTO>> {
   const parseResult = sessionFormSchema.safeParse(data);
   if (!parseResult.success) return ActionFailure(parseResult.error.errors.join(', '));
   const sessionParsedData = parseResult.data;
@@ -41,7 +43,7 @@ export async function createSessionAction(data: SessionForm): ActionResponse<Ses
   return ActionSuccess(sessionDTO, 'Session created successfully');
 }
 
-export async function getAllSessions(): ActionResponse<SessionDTO[]> {
+export async function getAllSessions(): Promise<ActionResponseProps<SessionDTO[]>> {
   const container = getContainer();
 
   const getAllSessions = container.session.GetAllSessionsUseCase;
@@ -66,7 +68,9 @@ export async function getAllSessions(): ActionResponse<SessionDTO[]> {
   };
 }
 
-export async function getSessionsByIds(ids: SessionProps['id'][]): ActionResponse<SessionDTO[]> {
+export async function getSessionsByIds(
+  ids: SessionProps['id'][]
+): Promise<ActionResponseProps<SessionDTO[]>> {
   const container = getContainer();
 
   const getAllSessions = container.session.GetSessionsByIdsUseCase;
@@ -91,7 +95,9 @@ export async function getSessionsByIds(ids: SessionProps['id'][]): ActionRespons
   };
 }
 
-export async function deleteSession(sessionId: SessionProps['id']): ActionResponse<SessionDTO> {
+export async function deleteSession(
+  sessionId: SessionProps['id']
+): Promise<ActionResponseProps<SessionDTO>> {
   const container = getContainer();
 
   const userIdResult = await getCurrentUserId();
