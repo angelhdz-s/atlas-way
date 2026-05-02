771f0889d639bdb6c35c15f9e55950f36d7c10fd
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-05-02 00:59:40 -0500

    refactor(ErrorMessage): improve types handling creating Props type and add undefined type as acceptable value

--- src/presentation/modules/form/components/ErrorMessage.tsx
+++ src/presentation/modules/form/components/ErrorMessage.tsx
@@ -1,4 +1,6 @@
-export function ErrorMessage({ message }: { message?: string }) {
+type Props = { message?: string | undefined };
+
+export function ErrorMessage({ message }: Props) {
   if (!message) return null;
   return <p className="text-danger leading-tight font-medium">{message}</p>;
 }
