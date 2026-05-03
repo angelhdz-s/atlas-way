344a36844e5c84ca8c4abaa8b222344ef9651e7f
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-05-03 00:27:55 -0500

    feat(mocks): add SelectOption mocks for form components unit tests

--- src/presentation/modules/form/components/SelectBoxOption.test.tsx
+++ src/presentation/modules/form/components/SelectBoxOption.test.tsx
@@ -2,52 +2,46 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { SelectBoxOption } from '@/presentation/modules/form/components/SelectBoxOption';
 import userEvent from '@testing-library/user-event';
-
-const options = [
-  {
-    label: 'Strength',
-    value: 'strength',
-  },
-  {
-    label: 'Health',
-    value: 'health',
-  },
-  {
-    label: 'Hypertrophy',
-    value: 'hypertrophy',
-  },
-];
+import { MOCK_SELECT_OPTIONS } from '@/shared/test/mocks/test.mocks.select-options';
 
 describe('<SelectBoxOption />', () => {
   describe('Basic Rendering', () => {
     it('Should be rendered with minimum props', () => {
-      render(<SelectBoxOption onSelect={() => {}} option={options[0]} />);
-      expect(screen.getByLabelText(options[0].label)).toBeInTheDocument();
+      render(<SelectBoxOption onSelect={() => {}} option={MOCK_SELECT_OPTIONS.STRENGTH} />);
+      expect(screen.getByLabelText(MOCK_SELECT_OPTIONS.STRENGTH.label)).toBeInTheDocument();
     });
   });
 
   describe('Integration with Props', () => {
     it('Should have radio input checked when isSelected prop provided as "true" ', () => {
-      render(<SelectBoxOption onSelect={() => {}} option={options[0]} isSelected />);
+      render(
+        <SelectBoxOption onSelect={() => {}} option={MOCK_SELECT_OPTIONS.STRENGTH} isSelected />
+      );
       expect(screen.getByRole('checkbox')).toBeChecked();
     });
 
     it('Should not have radio input checked when isSelected prop is not provided (false)', () => {
-      render(<SelectBoxOption onSelect={() => {}} option={options[0]} />);
+      render(<SelectBoxOption onSelect={() => {}} option={MOCK_SELECT_OPTIONS.STRENGTH} />);
       expect(screen.getByRole('checkbox')).not.toBeChecked();
     });
 
     it('Should update input check when rerendered', () => {
-      const { rerender } = render(<SelectBoxOption onSelect={() => {}} option={options[0]} />);
+      const { rerender } = render(
+        <SelectBoxOption onSelect={() => {}} option={MOCK_SELECT_OPTIONS.STRENGTH} />
+      );
       expect(screen.getByRole('checkbox')).not.toBeChecked();
-      rerender(<SelectBoxOption onSelect={() => {}} option={options[0]} isSelected />);
+      rerender(
+        <SelectBoxOption onSelect={() => {}} option={MOCK_SELECT_OPTIONS.STRENGTH} isSelected />
+      );
       expect(screen.getByRole('checkbox')).toBeChecked();
     });
 
     it('Should update checkbox by option prop when rerendered', () => {
-      const { rerender } = render(<SelectBoxOption onSelect={() => {}} option={options[0]} />);
+      const { rerender } = render(
+        <SelectBoxOption onSelect={() => {}} option={MOCK_SELECT_OPTIONS.STRENGTH} />
+      );
       expect(screen.getByRole('checkbox', { name: /strength/i })).toBeInTheDocument();
-      rerender(<SelectBoxOption onSelect={() => {}} option={options[1]} />);
+      rerender(<SelectBoxOption onSelect={() => {}} option={MOCK_SELECT_OPTIONS.HEALTH} />);
       expect(screen.queryByRole('checkbox', { name: /strength/i })).not.toBeInTheDocument();
       expect(screen.getByRole('checkbox', { name: /health/i })).toBeInTheDocument();
     });
@@ -58,8 +52,8 @@ describe('<SelectBoxOption />', () => {
       const fn = jest.fn();
       const user = userEvent.setup();
       const handleChange = (_v: string) => fn();
-      render(<SelectBoxOption onSelect={handleChange} option={options[0]} />);
-      await user.click(screen.getByLabelText(options[0].label));
+      render(<SelectBoxOption onSelect={handleChange} option={MOCK_SELECT_OPTIONS.STRENGTH} />);
+      await user.click(screen.getByLabelText(MOCK_SELECT_OPTIONS.STRENGTH.label));
       expect(fn).toHaveBeenCalledTimes(1);
     });
   });
@@ -67,19 +61,21 @@ describe('<SelectBoxOption />', () => {
   describe('Accessibility', () => {
     it('Should support keyboard navigation (focus)', async () => {
       const user = userEvent.setup();
-      render(<SelectBoxOption onSelect={() => {}} option={options[0]} />);
+      render(<SelectBoxOption onSelect={() => {}} option={MOCK_SELECT_OPTIONS.STRENGTH} />);
       await user.tab();
       expect(screen.getByRole('checkbox', { name: /strength/i })).toHaveFocus();
     });
 
     it('Should be highlighted when isSelected prop is "true"', async () => {
-      render(<SelectBoxOption onSelect={() => {}} option={options[0]} isSelected />);
-      expect(screen.getByText(options[0].label)).toHaveClass('border-success');
+      render(
+        <SelectBoxOption onSelect={() => {}} option={MOCK_SELECT_OPTIONS.STRENGTH} isSelected />
+      );
+      expect(screen.getByText(MOCK_SELECT_OPTIONS.STRENGTH.label)).toHaveClass('border-success');
     });
 
     it('Should not be highlighted when isSelected prop is not provided or as "false"', () => {
-      render(<SelectBoxOption onSelect={() => {}} option={options[0]} />);
-      expect(screen.getByText(options[0].label)).toHaveClass('border-bd-default');
+      render(<SelectBoxOption onSelect={() => {}} option={MOCK_SELECT_OPTIONS.STRENGTH} />);
+      expect(screen.getByText(MOCK_SELECT_OPTIONS.STRENGTH.label)).toHaveClass('border-bd-default');
     });
   });
 });
