fb7b78d5d038f7d337c28db35109bb4ed71ef4a0
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-05-04 18:08:13 -0500

    feat(GetUserByEmail): add unit tests

---
+++ src/modules/user/application/use-cases/get-user-by-email.test.ts
@@ -0,0 +1,56 @@
+import '@testing-library/jest-dom';
+import { Failure } from '@/shared/domain/result';
+import { TechnicalError } from '@/shared/domain/errors/domain.errors';
+import { InMemoryUserRepository } from '@/modules/user/mocks/user.mocks.repository';
+import { GetUserByEmail } from '@/modules/user/application/use-cases/get-user-by-email';
+import type { User } from '@/modules/user/domain/user.entity';
+
+describe('GetUserById use case', () => {
+  describe('Happy Path', () => {
+    it('should get user by email successfully', async () => {
+      // Set up
+      const userRepoMock = new InMemoryUserRepository();
+      const findByEmailSpy = jest.spyOn(userRepoMock, 'findByEmail');
+      const useCase = new GetUserByEmail(userRepoMock);
+
+      // Data
+      const user = userRepoMock.users[0] as User;
+
+      // Execute
+      const getUserByEmail = await useCase.execute(user.email);
+      const getUserByEmailNotFound = await useCase.execute('email.never@gmail.com');
+
+      // Assert interaction
+      expect(findByEmailSpy).toHaveBeenCalledTimes(2);
+
+      // Assert result pattern when user found
+      expect(getUserByEmail.success).toBe(true);
+      expect(getUserByEmail.success && getUserByEmail.data).toBe(user);
+      // Assert result pattern When user NOT found
+      expect(getUserByEmailNotFound.success).toBe(true);
+      expect(getUserByEmailNotFound.success && getUserByEmailNotFound.data).toBe(null);
+    });
+  });
+
+  describe('Dependency Interaction', () => {
+    it('should return failure when user findByEmail operation fails', async () => {
+      // Set up
+      const userRepoMock = new InMemoryUserRepository();
+      const findByEmailSpy = jest.spyOn(userRepoMock, 'findByEmail');
+      const useCase = new GetUserByEmail(userRepoMock);
+      jest
+        .spyOn(userRepoMock, 'findByEmail')
+        .mockResolvedValue(Failure(new TechnicalError() as never));
+
+      // Execute
+      const getUserByEmail = await useCase.execute('email.never@gmail.com');
+
+      // Assert interaction
+      expect(findByEmailSpy).toHaveBeenCalledTimes(1);
+
+      // Assert result pattern
+      expect(getUserByEmail.success).toBe(false);
+      expect(!getUserByEmail.success && getUserByEmail.error.code).toBe('TECHNICAL_ERROR');
+    });
+  });
+});
