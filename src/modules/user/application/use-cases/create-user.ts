f0be99d36ee6daaa0a929c6e466bbb92d975b852
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-05-04 03:51:13 -0500

    feat(CreateUser use-case): add missing validation of email duplication

--- src/modules/user/application/use-cases/create-user.ts
+++ src/modules/user/application/use-cases/create-user.ts
@@ -1,11 +1,12 @@
+import { Failure } from '@/shared/domain/result';
 import { User } from '@/modules/user/domain/user.entity';
 import { ROLES } from '@/modules/user/domain/user.constants.roles';
-import { Failure } from '@/shared/domain/result';
+import { TechnicalError } from '@/shared/domain/errors/domain.errors';
 import { RoleNotFound } from '@/modules/user/domain/errors/user.errors';
-import type { IUserRepository } from '@/modules/user/domain/user.repository';
-import type { CreateUserInput } from '@/modules/user/application/dtos/create-user.dto';
 import type { UseCase } from '@/shared/application/use-case';
 import type { IdGeneratorRepository } from '@/shared/application/id-generator';
+import type { IUserRepository } from '@/modules/user/domain/user.repository';
+import type { CreateUserInput } from '@/modules/user/application/dtos/create-user.dto';
 
 export class CreateUser implements UseCase {
   constructor(
@@ -15,10 +16,12 @@ export class CreateUser implements UseCase {
 
   async execute(data: CreateUserInput) {
     const id = this.generator.generate();
-
     const role = Object.values(ROLES).find((r) => r.id === data.roleId);
-
     if (!role) return Failure(new RoleNotFound());
+
+    const userByEmailResult = await this.repository.findByEmail(data.email);
+    if (!userByEmailResult.success) return userByEmailResult;
+    if (userByEmailResult.data) return Failure(new TechnicalError()); // more User exceptions needed
 
     const user = User.create(id, {
       email: data.email,
