47d0d4f91af77f9085cd76ebd533e79a686d4558
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-05-05 03:38:37 -0500

    refactor(user mocks): Add safety copy returns of in-memory user instances

--- src/modules/user/mocks/user.mocks.repository.ts
+++ src/modules/user/mocks/user.mocks.repository.ts
@@ -1,9 +1,9 @@
 import { Failure, Success } from '@/shared/domain/result';
+import { User } from '@/modules/user/domain/user.entity';
+import { UserNotFoundError } from '@/modules/user/domain/errors/user.errors';
 import { USERS_MOCK } from '@/modules/user/mocks/user.mocks.entities';
-import type { User } from '@/modules/user/domain/user.entity';
 import type { UserProps } from '@/modules/user/domain/user.types';
 import type { IUserRepository } from '@/modules/user/domain/user.repository';
-import { UserNotFoundError } from '../domain/errors/user.errors';
 
 /**
  * An in-memory mock implementation of IUserRepository for testing purposes.
@@ -17,20 +17,46 @@ export class InMemoryUserRepository implements IUserRepository {
     return Success(data);
   }
   async findAll() {
-    return Success(this.users);
+    const users = this.users;
+    const usersCopy = this.getCopies(users);
+    return Success(usersCopy);
   }
   async findByEmail(email: UserProps['email']) {
     const user = this.users.find((u) => u.email === email);
-    return Success(user ?? null);
+    if (!user) return Success(null);
+
+    const userCopy = this.getCopy(user);
+    return Success(userCopy);
   }
   async findById(id: UserProps['id']) {
     const user = this.users.find((u) => u.id === id);
-    return Success(user ?? null);
+    if (!user) return Success(null);
+
+    const userCopy = this.getCopy(user);
+    return Success(userCopy);
   }
   async update(data: User) {
     const userIndex = this.users.findIndex((u) => u.id === data.id);
     if (userIndex === -1) return Failure(new UserNotFoundError());
     this.users[userIndex] = data;
     return Success(data);
+  }
+
+  private getCopy(user: User): User {
+    const userProps: UserProps = {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      role: user.role,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
+
+    return new User(userProps);
+  }
+
+  private getCopies(users: User[]): User[] {
+    const userCopies = users.map((u) => this.getCopy(u));
+    return userCopies;
   }
 }
