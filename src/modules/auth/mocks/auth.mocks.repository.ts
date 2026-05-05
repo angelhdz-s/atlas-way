cf691e0a658a7a0af671b8f7aae3ebde16706f6f
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-05-05 01:55:07 -0500

    feat(auth mock): add MockAuthRepository class for testing

---
+++ src/modules/auth/mocks/auth.mocks.repository.ts
@@ -0,0 +1,14 @@
+import { Success } from '@/shared/domain/result';
+import type { RepositoryResult } from '@/shared/domain/repository.result';
+import type { IAuthRepository } from '@/modules/auth/domain/auth.respository';
+import type { AuthSession } from '@/modules/auth/domain/errors/auth-session.types';
+
+/**
+ * @property session: An `AuthSession` | `null` type to control fake-active session
+ */
+export class MockAuthRepository implements IAuthRepository {
+  public session: AuthSession | null = null;
+  async getSession(): RepositoryResult<AuthSession | null> {
+    return Success(this.session);
+  }
+}
