ae6fc2c3828b0afc12983465996aa7171eb2542a
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-05-06 01:27:13 -0500

    refactor(UUIDGenerator): update implementation to satisfy new IdGeneratorRepository contract

--- src/shared/infrastructure/generators/uuid-generator.ts
+++ src/shared/infrastructure/generators/uuid-generator.ts
@@ -1,7 +1,16 @@
+import { Failure, Success } from '@/shared/domain/result';
 import type { IdGeneratorRepository } from '@/shared/application/id-generator.repository';
+import type { InfrastructureErrorTranslator } from '@/shared/infrastructure/errors/error.translator';
 
 export class UUIDGenerator implements IdGeneratorRepository {
-  generate() {
-    return crypto.randomUUID();
+  constructor(readonly errorMapper: InfrastructureErrorTranslator) {}
+  async generate() {
+    try {
+      const id = crypto.randomUUID();
+      return Success(id);
+    } catch (e) {
+      const error = this.errorMapper.translate(e);
+      return Failure(error);
+    }
   }
 }
