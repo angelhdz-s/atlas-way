6b2a7d0a2f11489aae1dc514bb48ee25cd813710
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-05-06 01:28:16 -0500

    refactor(Login): update implementation to satisfy new IdGeneratorRepository contract

--- src/modules/auth/application/use-cases/login.ts
+++ src/modules/auth/application/use-cases/login.ts
@@ -18,10 +18,16 @@ export class Login implements UseCase {
     const sessionResult = await this.authRepository.getSession();
     if (!sessionResult.success) return sessionResult;
     if (sessionResult.data) return Failure(new SessionAlreadyActive());
+
     const userResult = await this.userRepository.findByEmail(data.email);
     if (!userResult.success) return userResult;
     if (userResult.data) return Success(null);
-    const userId = this.generatorRepository.generate();
+
+    const idResult = await this.generatorRepository.generate();
+    if(!idResult.success) return idResult;
+
+    const userId = idResult.data
+
     const domainUser = User.create(userId, {
       email: data.email,
       name: data.name,
@@ -30,6 +36,7 @@ export class Login implements UseCase {
 
     const createUserResult = await this.userRepository.create(domainUser);
     if (!createUserResult.success) return createUserResult;
+    
     return Success(null);
   }
 }
