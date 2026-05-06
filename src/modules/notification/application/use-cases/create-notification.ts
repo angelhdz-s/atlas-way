743552911c889788db6640536826c9cbb56b920a
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-05-06 00:45:42 -0500

    refactor(notification use-cases): update UseCase interface import

--- src/modules/notification/application/use-cases/create-notification.ts
+++ src/modules/notification/application/use-cases/create-notification.ts
@@ -1,5 +1,5 @@
 import { Notification } from '@/modules/notification/domain/notification.entity';
-import type { UseCase } from '@/shared/application/use-case';
+import type { UseCase } from '@/shared/application/shared.use-case';
 import type { INotificationRepository } from '@/modules/notification/domain/notification.repository';
 import type { CreateNotificationInput } from '@/modules/notification/application/dtos/create-notification.dto';
 import type { IdGeneratorRepository } from '@/shared/application/id-generator';
