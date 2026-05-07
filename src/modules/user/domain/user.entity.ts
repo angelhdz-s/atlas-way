6e00aa1c57640a59d8c3fb52063088c9af2f116c
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-05-07 00:37:47 -0500

    refactor(user entity): add result pattern and validations to User methods

--- src/modules/user/domain/user.entity.ts
+++ src/modules/user/domain/user.entity.ts
@@ -1,4 +1,14 @@
-import type { UserProps } from './user.types';
+import { Failure, Success } from '@/shared/domain/result';
+import { isValidUuid } from '@/shared/domain/validation.lib';
+import { InvalidUserData, RoleNotFound } from '@/modules/user/domain/errors/user.errors';
+import { ROLES } from '@/modules/user/domain/user.constants.roles';
+import {
+  isValidUserEmail,
+  isValidUserName,
+} from '@/modules/user/domain/validation/user.validation.lib';
+import type { Result } from '@/shared/domain/result';
+import type { DomainError } from '@/shared/domain/errors/domain.errors';
+import type { UserFactoryData, UserProps } from '@/modules/user/domain/user.types';
 
 export class User {
   constructor(private props: UserProps) {}
@@ -20,18 +30,32 @@ export class User {
   get role() {
     return this.props.role;
   }
-  changeName(name: UserProps['name']) {
+  changeName(name: UserProps['name']): Result<null, DomainError> {
+    if (!isValidUserName(name)) return Failure(new InvalidUserData('NAME'));
     this.props.name = name;
+    return Success(null);
   }
-  changeEmail(email: UserProps['email']) {
+  changeEmail(email: UserProps['email']): Result<null, DomainError> {
+    if (!isValidUserEmail(email)) return Failure(new InvalidUserData('EMAIL'));
     this.props.email = email;
+    return Success(null);
   }
-  static create(id: UserProps['id'], data: Omit<UserProps, 'id' | 'createdAt' | 'updatedAt'>) {
-    return new User({
+  static create(id: UserProps['id'], data: UserFactoryData): Result<User, DomainError> {
+    if (!isValidUuid(id)) return Failure(new InvalidUserData());
+    if (!isValidUserEmail(data.email)) return Failure(new InvalidUserData('EMAIL'));
+    if (!isValidUserName(data.name)) return Failure(new InvalidUserData('NAME'));
+
+    const role = Object.values(ROLES).find((r) => r.id === data.roleId);
+    if (!role) return Failure(new RoleNotFound());
+
+    const newUser = new User({
       ...data,
+      role,
       createdAt: new Date(),
       updatedAt: new Date(),
       id,
     });
+
+    return Success(newUser);
   }
 }
