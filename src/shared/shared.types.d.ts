45386f96a565f9dba0ad0c7a37a7fcf262a75b48
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-05-08 17:12:32 -0500

    feat(types): add standard types for domain entities and methods

--- src/shared/shared.types.d.ts
+++ src/shared/shared.types.d.ts
@@ -1,1 +1,9 @@
+import type { Result } from '@/shared/domain/result';
+
 export type NonEmptyString<T extends string> = T extends '' ? never : T;
+
+export type EntityMethod = (...args: any[]) => Result<any, DomainError>;
+
+export type ClassMethods<T> = {
+  [K in keyof T as T[K] extends EntityMethod ? K : never]: T[K];
+};
