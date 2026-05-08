8e65e90735abadfbe37951cb2304a93dd19ab7bc
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-05-08 17:21:46 -0500

    refactor(UpdateUser): add updateUserProperties helper to use case logic

--- src/modules/user/application/use-cases/update-user.ts
+++ src/modules/user/application/use-cases/update-user.ts
@@ -1,5 +1,6 @@
-import { Failure, Success } from '@/shared/domain/result';
+import { Failure } from '@/shared/domain/result';
 import { UserNotFoundError } from '@/modules/user/domain/errors/user.errors';
+import { updateUserProperties } from '@/modules/user/application/helpers/update-user.use-case.helpers';
 import type { UseCase } from '@/shared/application/shared.use-case';
 import type { UserProps } from '@/modules/user/domain/user.types';
 import type { IUserRepository } from '@/modules/user/domain/user.repository';
@@ -15,11 +16,8 @@ export class UpdateUser implements UseCase {
     if (!userResult.data) return Failure(new UserNotFoundError());
     const user = userResult.data;
 
-    const nameResult = data.name ? user.changeName(data.name) : Success(null);
-    if (!nameResult.success) return nameResult;
-
-    const emailResult = data.email ? user.changeEmail(data.email) : Success(null);
-    if (!emailResult.success) return emailResult;
+    const updateUserResult = updateUserProperties(data, user);
+    if (!updateUserResult.success) return updateUserResult;
 
     return await this.repository.update(user);
   }
