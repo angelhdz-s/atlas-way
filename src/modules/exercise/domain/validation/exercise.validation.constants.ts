23d51ef75226780d9af84e0bb3346143f0e3e7d2
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-05-12 03:10:50 -0500

    feat(exercise domain): add validation constants

---
+++ src/modules/exercise/domain/validation/exercise.validation.constants.ts
@@ -0,0 +1,21 @@
+export const EXERCISE_NAME_LENGTH = {
+  min: 3,
+  max: 30,
+} as const;
+
+export const EXERCISE_DESCRIPTION_LENGTH = {
+  min: 0,
+  max: 100,
+} as const;
+
+export const EXERCISE_SETS_RANGE = {
+  min: 1,
+} as const;
+
+export const EXERCISE_REPS_RANGE = {
+  min: 1,
+} as const;
+
+export const EXERCISE_WEIGHT_RANGE = {
+  min: 0,
+} as const;
