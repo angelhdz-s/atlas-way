fe97b0daa02828e9aa6bf34f552bcfa55d629211
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-05-12 20:09:42 -0500

    feat(exercise entity): add result pattern and validations in update methods

--- src/modules/exercise/domain/exercise.entity.ts
+++ src/modules/exercise/domain/exercise.entity.ts
@@ -1,5 +1,9 @@
+import type { Result } from '@/shared/domain/result';
 import type { ExerciseFactoryData, ExerciseProps } from '@/modules/exercise/domain/exercise.types';
 import type { Muscle } from '@/modules/muscle/domain/muscle.entity';
+import type { DomainError } from '@/shared/domain/errors/domain.errors';
+import { Failure, Success } from '@/shared/domain/result';
+import { exerciseValidators } from '@/modules/exercise/domain/validation/exercise.validation';
 
 export class Exercise {
   constructor(private data: ExerciseProps) {}
@@ -33,23 +37,36 @@ export class Exercise {
   get muscles() {
     return this.data.muscles;
   }
-  changeName(name: ExerciseProps['name']) {
-    this.data.name = name;
+  changeName(name: ExerciseProps['name']): Result<null, DomainError> {
+    if (!exerciseValidators.name.validate(name)) return Failure(exerciseValidators.name.error);
+    this.data.name = name.trim();
+    return Success(null);
   }
-  changeDescription(description: ExerciseProps['description']) {
-    this.data.description = description;
+  changeDescription(description: ExerciseProps['description']): Result<null, DomainError> {
+    if (!exerciseValidators.description.validate(description))
+      return Failure(exerciseValidators.description.error);
+    this.data.description = description ? description.trim() : null;
+    return Success(null);
   }
-  changeSets(sets: ExerciseProps['sets']) {
+  changeSets(sets: ExerciseProps['sets']): Result<null, DomainError> {
+    if (!exerciseValidators.sets.validate(sets)) return Failure(exerciseValidators.sets.error);
     this.data.sets = sets;
+    return Success(null);
   }
-  changeReps(reps: ExerciseProps['reps']) {
+  changeReps(reps: ExerciseProps['reps']): Result<null, DomainError> {
+    if (!exerciseValidators.reps.validate(reps)) return Failure(exerciseValidators.reps.error);
     this.data.reps = reps;
+    return Success(null);
   }
-  changeWeight(weight: ExerciseProps['weight']) {
+  changeWeight(weight: ExerciseProps['weight']): Result<null, DomainError> {
+    if (!exerciseValidators.weight.validate(weight))
+      return Failure(exerciseValidators.weight.error);
     this.data.weight = weight;
+    return Success(null);
   }
-  changeMuscles(muscles: Muscle[]) {
+  changeMuscles(muscles: Muscle[]): Result<null, DomainError> {
     this.data.muscles = muscles;
+    return Success(null);
   }
 
   static create(id: ExerciseProps['id'], data: ExerciseFactoryData) {
