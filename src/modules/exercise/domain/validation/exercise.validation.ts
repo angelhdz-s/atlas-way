6b8a5e69f3bef2a055eaee6e86581c58e2a3a81a
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-05-12 20:52:13 -0500

    feat(exercise errors): add errors in field validators

--- src/modules/exercise/domain/validation/exercise.validation.ts
+++ src/modules/exercise/domain/validation/exercise.validation.ts
@@ -1,6 +1,5 @@
 import { Failure, Success } from '@/shared/domain/result';
 import { isKeyOf, isObject, isValidUuid } from '@/shared/domain/validation.utils';
-import { TechnicalError } from '@/shared/domain/errors/domain.errors';
 import { Exercise } from '@/modules/exercise/domain/exercise.entity';
 import {
   isValidExerciseDescription,
@@ -9,6 +8,7 @@ import {
   isValidExerciseSets,
   isValidExerciseWeight,
 } from '@/modules/exercise/domain/validation/exercise.validation.utils';
+import { InvalidExerciseData } from '@/modules/exercise/domain/errors/exercise.errors';
 import type { ValidationFunction } from '@/shared/shared.types';
 import type { Result } from '@/shared/domain/result';
 import type { DomainError } from '@/shared/domain/errors/domain.errors';
@@ -27,36 +27,36 @@ type ExerciseValidators = {
 export const exerciseValidators: ExerciseValidators = {
   id: {
     validate: isValidUuid,
-    error: new TechnicalError(),
+    error: new InvalidExerciseData(),
   },
   name: {
     validate: isValidExerciseName,
-    error: new TechnicalError(),
+    error: new InvalidExerciseData('NAME'),
   },
   description: {
     validate: isValidExerciseDescription,
-    error: new TechnicalError(),
+    error: new InvalidExerciseData('DESCRIPTION'),
   },
   sets: {
     validate: isValidExerciseSets,
-    error: new TechnicalError(),
+    error: new InvalidExerciseData('SETS'),
   },
   reps: {
     validate: isValidExerciseReps,
-    error: new TechnicalError(),
+    error: new InvalidExerciseData('REPS'),
   },
   weight: {
     validate: isValidExerciseWeight,
-    error: new TechnicalError(),
+    error: new InvalidExerciseData('WEIGHT'),
   },
 };
 
 export type ExerciseMinimumProps = Omit<ExerciseProps, 'createdAt' | 'updatedAt'>;
 export function validateExercise(exercise: ExerciseMinimumProps): Result<Exercise, DomainError> {
-  if (!isObject(exercise)) return Failure(new TechnicalError());
+  if (!isObject(exercise)) return Failure(new InvalidExerciseData());
   const exerciseValidationKeys = Object.keys(exerciseValidators) as (keyof ExerciseValidators)[];
   for (const key of exerciseValidationKeys) {
-    if (!isKeyOf(key, exercise)) return Failure(new TechnicalError());
+    if (!isKeyOf(key, exercise)) return Failure(new InvalidExerciseData());
     const validate = exerciseValidators[key].validate;
     if (!validate(exercise[key])) return Failure(exerciseValidators[key].error);
   }
