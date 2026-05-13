a25ae817180b955de71c133c8f8fd5a747b28a95
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-05-12 20:49:00 -0500

    feat(exercise domain): add new errors

--- src/modules/exercise/domain/errors/exercise.error.code.ts
+++ src/modules/exercise/domain/errors/exercise.error.code.ts
@@ -1,1 +1,7 @@
-export type ExerciseErrorCode = 'EXERCISE_NOT_FOUND' | 'EXERCISE_OWNERSHIP_ERROR';
+export type ExerciseInvalidDataErrorCodes = 'NAME' | 'DESCRIPTION' | 'SETS' | 'REPS' | 'WEIGHT';
+
+export type ExerciseErrorCode =
+  | 'EXERCISE_NOT_FOUND'
+  | 'EXERCISE_OWNERSHIP_ERROR'
+  | 'INVALID_EXERCISE_DATA'
+  | `INVALID_EXERCISE_DATA.${ExerciseInvalidDataErrorCodes}`;
--- src/modules/exercise/domain/errors/exercise.errors.ts
+++ src/modules/exercise/domain/errors/exercise.errors.ts
@@ -1,3 +1,4 @@
+import type { ExerciseInvalidDataErrorCodes } from '@/modules/exercise/domain/errors/exercise.error.code';
 import { DomainError } from '@/shared/domain/errors/domain.errors';
 
 export class ExerciseNotFoundError extends DomainError {
@@ -9,5 +10,15 @@ export class ExerciseNotFoundError extends DomainError {
 export class ExerciseOwnershipError extends DomainError {
   constructor() {
     super('EXERCISE_OWNERSHIP_ERROR');
+  }
+}
+
+export class InvalidExerciseData extends DomainError {
+  constructor(readonly error_code: ExerciseInvalidDataErrorCodes | undefined = undefined) {
+    if (!error_code) {
+      super('INVALID_EXERCISE_DATA');
+    } else {
+      super(`INVALID_EXERCISE_DATA.${error_code}`);
+    }
   }
 }
