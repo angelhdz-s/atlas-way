fc1b20a773db367f877cc26eda2986d62f92c432
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-05-16 03:03:39 -0500

    refactor(chore auth): remove @react-doctor warning for public server action getSession()

--- src/modules/auth/presentation/auth.actions.ts
+++ src/modules/auth/presentation/auth.actions.ts
@@ -5,11 +5,14 @@ import type { AuthSession } from '@/modules/auth/domain/auth-session.types';
 import { getContainer } from '@/di/containers';
 import { ActionFailure, ActionSuccess } from '@/shared/presentation/action.response';
 
+// This server action is the only one public
+// react-doctor-disable-next-line react-doctor/server-auth-actions
 export async function getSession(): Promise<ActionResponseProps<AuthSession | null>> {
   const container = getContainer();
   const getCurrentSession = container.auth.GetCurrentSessionUseCase;
   const sessionResult = await getCurrentSession.execute();
   if (!sessionResult.success) return ActionFailure(sessionResult.error.message);
+
   return ActionSuccess(sessionResult.data, 'Session obtained successfully');
 }
 
@@ -21,5 +24,6 @@ export async function logout(): Promise<ActionResponseProps<null>> {
   const logout = container.auth.LogoutUseCase;
   const logoutResult = await logout.execute();
   if (!logoutResult.success) return ActionFailure(logoutResult.error.message);
+
   return ActionSuccess(null, 'Logged out successfully');
 }
