d4a4b50276bf8e2eaeb6671a9332a923b9d5ac93
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-05-18 20:14:32 -0500

    refactor(session): update create and update use cases logic

--- src/modules/session/application/use-cases/update-session.ts
+++ src/modules/session/application/use-cases/update-session.ts
@@ -1,20 +1,12 @@
-import type { Result } from '@/shared/domain/result';
-import type { DomainError } from '@/shared/domain/errors/domain.errors';
 import type { IExerciseRepository } from '@/modules/exercise/domain/exercise.repository';
 import type { ISessionRepository } from '@/modules/session/domain/session.repository';
 import type { SessionProps } from '@/modules/session/domain/session.types';
 import type { UpdateSessionInput } from '@/modules/session/application/dtos/update-session.dto';
 import type { UseCase } from '@/shared/application/shared.use-case';
-import type { Exercise } from '@/modules/exercise/domain/exercise.entity';
-import { Failure, Success } from '@/shared/domain/result';
+import { Failure } from '@/shared/domain/result';
 import { SessionNotFoundError } from '@/modules/session/domain/errors/session.errors';
-import {
-  ExerciseNotFoundError,
-  InvalidExerciseData,
-} from '@/modules/exercise/domain/errors/exercise.errors';
-import { isArray } from '@/shared/domain/validation/validation.non-primitives';
-import { isArrayOf } from '@/shared/domain/validation/validation.utils';
 import { updateSessionProperties } from '@/modules/session/application/helpers/update-session.use-case.helpers';
+import { ExerciseValidationService } from '@/modules/exercise/domain/services/exercise.validation.service';
 
 export class UpdateSession implements UseCase {
   constructor(
@@ -32,27 +24,13 @@ export class UpdateSession implements UseCase {
     const updatePropsResult = updateSessionProperties(data, session);
     if (!updatePropsResult.success) return updatePropsResult;
 
-    const exercisesResult = await this.prepareExercises(data.exerciseIds);
-    if (!exercisesResult.success) return exercisesResult;
-    if (exercisesResult.data) session.changeExercises(exercisesResult.data);
+    const exerciseValidationService = new ExerciseValidationService(this.exerciseRepository);
+    const exerciseValidatioResult = await exerciseValidationService.validateAndFetch(
+      data.exerciseIds
+    );
+    if (!exerciseValidatioResult.success) return exerciseValidatioResult;
+    if (exerciseValidatioResult.data) session.changeExercises(exerciseValidatioResult.data);
 
     return await this.sessionRepository.update(session);
-  }
-
-  private async prepareExercises(
-    exerciseIds?: UpdateSessionInput['exerciseIds']
-  ): Promise<Result<Exercise[] | null, DomainError>> {
-    if (!exerciseIds) return Success(null);
-    if (!isArray(exerciseIds) || !isArrayOf(exerciseIds, 'string'))
-      return Failure(new InvalidExerciseData());
-    const exercises: Exercise[] = [];
-    const exercisesFindById = exerciseIds.map((id) => this.exerciseRepository.findById(id));
-    const exercisesResults = await Promise.all(exercisesFindById);
-    for (const exerciseResult of exercisesResults) {
-      if (!exerciseResult.success) return exerciseResult;
-      if (!exerciseResult.data) return Failure(new ExerciseNotFoundError());
-      exercises.push(exerciseResult.data);
-    }
-    return Success(exercises);
   }
 }
