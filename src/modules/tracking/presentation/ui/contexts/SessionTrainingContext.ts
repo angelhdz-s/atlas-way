de8d17c1840cf8e5d906f309318fdc74b4bb23c5
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-07-12 03:01:51 -0500

    feat(tracking): add session training provider

---
+++ src/modules/tracking/presentation/ui/contexts/SessionTrainingContext.ts
@@ -0,0 +1,61 @@
+import type { ExerciseDTO } from '@/modules/exercise/application/dtos/exercise.dto';
+import type { SessionTrainingSetForm } from '@/modules/tracking/presentation/tracking.presentation.types';
+import { createContext } from 'react';
+
+export type TrainingStep = {
+  id: string;
+  title: string;
+  set: number;
+  reps: number;
+  rir: number;
+  weight: number;
+  status: 'pending' | 'complete' | 'error' | 'cancel';
+};
+
+export type TrainingStage = {
+  id: string;
+  title: string;
+  stage: number;
+  status: 'pending' | 'complete' | 'cancel' | 'error';
+  steps: TrainingStep[];
+};
+
+export type TrainingState = {
+  lastStep: number;
+  length: number;
+  stages: TrainingStage[];
+};
+
+export type SessionTrainingContextType = {
+  targets: ExerciseDTO[];
+  stepIndex: number;
+  stageIndex: number;
+  currentStep: {
+    step: number;
+    stage: number;
+    accumulatedStep: number;
+  };
+  trainingState: TrainingState;
+  nextStep: () => void;
+  previousStep: () => void;
+  updateCurrentStage: (currentStepValues: SessionTrainingSetForm, error?: boolean) => void;
+};
+
+export const SessionTrainingContext = createContext<SessionTrainingContextType>({
+  targets: [],
+  stepIndex: 0,
+  stageIndex: 0,
+  currentStep: {
+    accumulatedStep: 1,
+    stage: 1,
+    step: 1,
+  },
+  trainingState: {
+    length: 1,
+    lastStep: 1,
+    stages: [],
+  },
+  nextStep: () => {},
+  previousStep: () => {},
+  updateCurrentStage: () => {},
+});
