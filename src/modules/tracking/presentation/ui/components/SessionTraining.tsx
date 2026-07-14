1633d130aa49280301c06396a212e2d6d66c5ad2
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-07-13 20:17:10 -0500

    feat(tracking): add the new created SessionTrainingFormProvider component

--- src/modules/tracking/presentation/ui/components/SessionTraining.tsx
+++ src/modules/tracking/presentation/ui/components/SessionTraining.tsx
@@ -1,15 +1,14 @@
 'use client';
 
 import { twMerge } from 'tailwind-merge';
-import { Button } from '@/presentation/modules/button/components/Button';
 import { PageContainer } from '@/presentation/modules/dashboard/components/page/PageContainer';
 import { PageContent } from '@/presentation/modules/dashboard/components/page/PageContent';
 import { PageHeader } from '@/presentation/modules/dashboard/components/page/PageHeader';
 import { SessionTrainingHeaderSteps } from '@/modules/tracking/presentation/ui/components/session-training/SessionTrainingHeaderSteps';
 import { SessionTrainingAsideData } from '@/modules/tracking/presentation/ui/components/session-training/SessionTrainingAsideData';
-import { useSessionTraining } from '@/modules/tracking/presentation/ui/hooks/useSessionTraining';
 import { SessionTrainingFormWrapper } from '@/modules/tracking/presentation/ui/components/session-training/SessionTrainingFormWrapper';
-import { FormProvider } from 'react-hook-form';
+import { SessionTrainingFormProvider } from '@/modules/tracking/presentation/ui/components/session-training/SessionTrainingFormProvider';
+import { SessionTrainingButtons } from '@/modules/tracking/presentation/ui/components/session-training/SessionTrainingButtons';
 
 type Props = {
   className?: string;
@@ -20,13 +19,15 @@ export function SessionTraining({ className }: Props) {
     <PageContainer className={twMerge('flex flex-row gap-8', className)}>
       <div className="flex-1 space-y-8">
         <PageHeader title="Training" description="Let's train" />
-        <PageContent className="space-y-8">
-          <SessionTrainingHeaderSteps />
-          <main className="min-h-24">
+        <SessionTrainingFormProvider>
+          <PageContent className="space-y-8">
+            <SessionTrainingHeaderSteps />
+            <main className="min-h-24">
               <SessionTrainingFormWrapper />
-          </main>
+            </main>
             <SessionTrainingButtons />
-        </PageContent>
+          </PageContent>
+        </SessionTrainingFormProvider>
       </div>
 
       <aside className="w-100 space-y-8">
