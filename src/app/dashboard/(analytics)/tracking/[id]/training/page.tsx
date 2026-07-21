9d9211cf93a637dc726a4e6c2ac2abdfc18b71c5
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-07-20 20:20:17 -0500

    feat(training page): add id param types and validation logic

--- src/app/dashboard/(analytics)/tracking/[id]/training/page.tsx
+++ src/app/dashboard/(analytics)/tracking/[id]/training/page.tsx
@@ -1,23 +1,57 @@
-import { getAllRoutines } from '@/modules/routine/presentation/routine.actions';
-import { SessionTraining } from '@/modules/tracking/presentation/ui/components/SessionTraining';
-import { SessionTrainingStepsProvider } from '@/modules/tracking/presentation/ui/components/SessionTrainingStepsProvider';
+import { RoutineEmptyData } from '@/modules/routine/presentation/ui/components/RoutineEmptyData';
+import { SessionEmptyData } from '@/modules/session/presentation/ui/components/SessionEmptyData';
+import {
+  getTrainingById,
+  getTrainingPlansByTrainingId,
+} from '@/modules/tracking/presentation/tracking.actions';
+import { Training } from '@/modules/tracking/presentation/ui/components/Training';
+import { TrainingStepsProvider } from '@/modules/tracking/presentation/ui/components/training/TrainingStepsProvider';
 
-export default async function TrackingTrainingPage() {
-  const routinesResult = await getAllRoutines();
-  const routine = routinesResult.success ? routinesResult.data[0] : null;
-  if (!routine) return null;
-  const plan = routine.plan.find((p) => p.session !== null);
-  if (!plan) return null;
+export default async function TrackingTrainingPage(
+  pageParams: Promise<{ params: Promise<{ id: string }> }>
+) {
+  const syncPageParams = await pageParams;
+  const syncParams = await syncPageParams.params;
 
-  const { session } = plan;
-  if (!session) return null;
+  if (!syncParams.id) {
+    console.log('No id parameter found');
+    return <RoutineEmptyData />;
+  }
 
-  const exercise = session.exercises[0];
-  if (!exercise) return null;
+  const trainingResult = await getTrainingById(syncParams.id);
+  if (!trainingResult.success) {
+    return <RoutineEmptyData />;
+  }
+
+  if (!trainingResult.data) {
+    console.log('Training not found');
+    return <RoutineEmptyData />;
+  }
+
+  if (
+    trainingResult.data?.statusId !== 'TARGETS_SET' &&
+    trainingResult.data?.statusId !== 'IN_PROGRESS'
+  ) {
+    console.log(`Training status is ${trainingResult.data.statusId}`);
+    return <RoutineEmptyData />;
+  }
+
+  const trainingPlansResult = await getTrainingPlansByTrainingId(syncParams.id);
+
+  if (!trainingPlansResult.success) {
+    return <RoutineEmptyData />;
+  }
+
+  if (trainingPlansResult.data.length < 0) {
+    console.log('No training plans found');
+    return <SessionEmptyData />;
+  }
+
+  const targets = trainingPlansResult.data;
 
   return (
-    <SessionTrainingStepsProvider targets={session.exercises}>
-      <SessionTraining />
-    </SessionTrainingStepsProvider>
+    <TrainingStepsProvider targets={targets}>
+      <Training />
+    </TrainingStepsProvider>
   );
 }
