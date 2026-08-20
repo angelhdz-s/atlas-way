c6c3c6df2c5fa028ee3ee607bf8f74a5a9b9b7e8
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-08-19 21:53:56 -0500

    feat(StepFormProvider): add zod schema support for Form validation

--- src/presentation/modules/wizard/components/StepFormProvider.tsx
+++ src/presentation/modules/wizard/components/StepFormProvider.tsx
@@ -1,14 +1,22 @@
 'use client';
 
+import { zodResolver } from '@hookform/resolvers/zod';
 import { FormProvider, useForm } from 'react-hook-form';
+import type { ZodSchema } from 'zod/v3';
 
-type Props<FormValues> = {
+type Props<FormValues, Schema extends ZodSchema<any, any>> = {
   children: React.ReactNode;
   defaultValues: Record<string, FormValues>;
+  schema: Schema;
 };
 
-export function StepFormProvider<FormValues>({ children, defaultValues }: Props<FormValues>) {
-  const methods = useForm<Props<FormValues>['defaultValues']>({
+export function StepFormProvider<FormValues, Schema extends ZodSchema<any, any>>({
+  children,
+  defaultValues,
+  schema,
+}: Props<FormValues, Schema>) {
+  const methods = useForm<Props<FormValues, Schema>['defaultValues']>({
+    resolver: zodResolver(schema),
     defaultValues,
     mode: 'onTouched',
   });
