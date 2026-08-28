f1bd9655536c93f09d2d2c221cf771e3d6a3522a
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-08-28 03:00:59 -0500

    feat(TrainingHeaderSteps): add new header and subheader content for phase information

--- src/modules/tracking/presentation/ui/components/training/TrainingHeaderSteps.tsx
+++ src/modules/tracking/presentation/ui/components/training/TrainingHeaderSteps.tsx
@@ -1,10 +1,9 @@
 'use client';
 
-// import { twMerge } from 'tailwind-merge';
-// import { IconCheck, IconClock, IconX } from '@tabler/icons-react';
-// import { useTrainingSteps } from '@/modules/tracking/presentation/ui/hooks/useTrainingSteps';
-
+import type { SetForm } from '@/modules/tracking/presentation/schemas/training.schema';
 import { useStepEngine } from '@/presentation/modules/wizard/hooks/useStepEngine';
+import { useWizardSummary } from '@/presentation/modules/wizard/hooks/useWizardSummary';
+import { twMerge } from 'tailwind-merge';
 
 type Props = {
   className?: string;
@@ -12,55 +11,14 @@ type Props = {
 
 export function TrainingHeaderSteps({ className }: Props) {
   const { currentStep } = useStepEngine();
+  const { phases } = useWizardSummary<SetForm>();
 
-  return null;
+  const currentPhase = phases.find((p) => p.phaseId === currentStep.phase.id);
 
-  // return (
-  //   <header className={twMerge('space-y-8', className)}>
-  //     <ul className="flex items-center gap-2">
-  //       {trainingState.stages.map((s, i) => (
-  //         <li key={s.id}>
-  //           <button
-  //             type="button"
-  //             className={twMerge(
-  //               'flex size-8 w-fit cursor-pointer items-center justify-center gap-2 rounded-full px-2',
-  //               s.stage === currentStep.stage ? 'bg-primary text-fg-strong' : 'bg-fill-base'
-  //             )}
-  //           >
-  //             {`Step ${i + 1}`}
-  //             {s.status === 'PENDING' && <IconClock size={16} strokeWidth={2} />}
-  //             {s.status === 'COMPLETED' && <IconCheck size={16} strokeWidth={2} />}
-  //             {s.status === 'CANCELED' && s.steps.length > 0 && (
-  //               <IconCheck size={16} strokeWidth={2} />
-  //             )}
-  //             {s.status === 'CANCELED' && s.steps.length === 0 && (
-  //               <IconX size={16} strokeWidth={2} />
-  //             )}
-  //           </button>
-  //         </li>
-  //       ))}
-  //     </ul>
-
-  //     {targets[stageIndex] !== undefined && (
-  //       <div
-  //         key={targets[stageIndex].id}
-  //         className={twMerge('animate-duration-100', 'animate-fade-left')}
-  //       >
-  //         <h6>{targets[stageIndex].exercise.name}</h6>
-  //         <p>{targets[stageIndex].exercise.description ?? 'No description.'}</p>
-  //       </div>
-  //     )}
-
-  //     <div className="flex items-center gap-8">
-  //       <div className="w-20">
-  //         <h4>Stage</h4>
-  //         <p>{currentStep.stage}</p>
-  //       </div>
-  //       <div className="w-20">
-  //         <h4>Step</h4>
-  //         <p>{currentStep.step}</p>
-  //       </div>
-  //     </div>
-  //   </header>
-  // );
+  return (
+    <header className={twMerge('', className)}>
+      <h5>{currentPhase?.title ?? 'No phase title'}</h5>
+      <p>{currentPhase?.description ?? 'No phase description'}</p>
+    </header>
+  );
 }
