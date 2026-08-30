851b112eb6d3dc6034638b63da59a6dd09183765
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-08-30 03:07:12 -0500

    feat(TrainingButtons): add the new wizard logic to navigation buttons

--- src/modules/tracking/presentation/ui/components/training/TrainingButtons.tsx
+++ src/modules/tracking/presentation/ui/components/training/TrainingButtons.tsx
@@ -1,48 +1,47 @@
 'use client';
 
+import type { SetForm } from '@/modules/tracking/presentation/schemas/training.schema';
 import { Button } from '@/presentation/modules/button/components/Button';
 import { useStepEngine } from '@/presentation/modules/wizard/hooks/useStepEngine';
 import { useStepFormSync } from '@/presentation/modules/wizard/hooks/useStepFormSync';
-// import { useState } from 'react';
+import { useWizard } from '@/presentation/modules/wizard/hooks/useWizard';
 import { twMerge } from 'tailwind-merge';
 
 type Props = {
   className?: string;
 };
 
-// const invalidExerciseStatus: TrainingStage['status'][] = ['COMPLETED', 'CANCELED'];
-
 export function TrainingButtons({ className }: Props) {
-  const { isLastStep, isFirstStep } = useStepEngine();
-  const { handleNext, handlePrev, isSaving } = useStepFormSync();
+  const { isLastStep, isFirstStep, cancelledPhaseIds, currentStep } = useStepEngine();
+  const { isSaving } = useStepFormSync();
 
-  // const [isShowingConfirmation, setIsShowingConfirmation] = useState<boolean>(false);
+  const { goToNextStep, goToPrevStep, toggleCancelPhase } = useWizard<SetForm>();
+
+  const isPhaseCancelled = cancelledPhaseIds.has(currentStep.phase.id);
 
   return (
     <>
       <footer
         className={twMerge('flex items-center justify-between gap-8', 'justify-end', className)}
       >
-        {/* {canLeaveExercise && (
-          <div>
-            <Button
-              key="target-next-button"
-              variant={{ color: 'simple' }}
-              onClick={() => setIsShowingConfirmation(true)}
-            >
-              Finish exercise
-            </Button>
-          </div>
-        )} */}
+        <div>
+          <Button
+            key="target-next-button"
+            variant={{ color: 'simple' }}
+            onClick={() => toggleCancelPhase(currentStep.phase.id)}
+          >
+            {isPhaseCancelled ? 'Retake phase' : 'End phase'}
+          </Button>
+        </div>
         <main className="flex items-center gap-4">
-          <Button key="target-previous-button" variant={{ color: 'subtle' }} onClick={handlePrev}>
+          <Button key="target-previous-button" variant={{ color: 'subtle' }} onClick={goToPrevStep}>
             {isFirstStep ? 'Cancel' : 'Previous set'}
           </Button>
           <Button
             key="target-next-button"
             variant={{ color: 'primary' }}
             disabled={isSaving}
-            onClick={handleNext}
+            onClick={goToNextStep}
           >
             {isLastStep ? 'Finish training' : 'Next set'}
           </Button>
