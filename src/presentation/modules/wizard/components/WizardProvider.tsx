dab7d4c3c31e4ff92a1d314ae20fb7145a407fdb
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-08-31 02:52:06 -0500

    feat(WizardProvider): use Different generic type utility to assert server action contract

--- src/presentation/modules/wizard/components/WizardProvider.tsx
+++ src/presentation/modules/wizard/components/WizardProvider.tsx
@@ -5,15 +5,18 @@ import { WizardOrchestratorInner } from '@/presentation/modules/wizard/component
 import { WizardSummaryProvider } from '@/presentation/modules/wizard/components/WizardSummaryProvider';
 import type { FlatStep, FlatStepWithText } from '@/presentation/modules/wizard/wizard.types';
 import type { ActionResponseProps } from '@/shared/presentation/action.response';
+import type { Different } from '@/shared/shared.types';
 import type { ZodSchema } from 'zod/v3';
 
-type Props<Schema extends ZodSchema<any, any>, FormValues> = {
+type Props<Schema extends ZodSchema<any, any>, FormValues, FormEntityDTO> = {
   children: React.ReactNode;
   flatSteps: FlatStep[];
   flatStepsWithText: FlatStepWithText[];
   formSchema: Schema;
   formDefaultValues: Record<string, FormValues>;
-  processSetFormData: (data: FormValues) => Promise<ActionResponseProps<FormValues>>;
+  processSetFormData: (
+    data: FormValues
+  ) => Promise<ActionResponseProps<Different<FormEntityDTO, FormValues>>>;
   populateNextPhaseStep: (data: {
     currentStepValue: FormValues;
     nextStepValue: FormValues;
@@ -21,7 +24,7 @@ type Props<Schema extends ZodSchema<any, any>, FormValues> = {
   isFormStepCompleted: (data: FormValues) => boolean;
 };
 
-export function WizardProvider<Schema extends ZodSchema<any, any>, FormValues>({
+export function WizardProvider<Schema extends ZodSchema<any, any>, FormValues, FormEntityDTO>({
   children,
   flatSteps,
   flatStepsWithText,
@@ -30,7 +33,7 @@ export function WizardProvider<Schema extends ZodSchema<any, any>, FormValues>({
   processSetFormData,
   populateNextPhaseStep,
   isFormStepCompleted,
-}: Props<Schema, FormValues>) {
+}: Props<Schema, FormValues, FormEntityDTO>) {
   return (
     <StepEngineProvider flatSteps={flatSteps}>
       <StepFormProvider schema={formSchema} defaultValues={formDefaultValues}>
