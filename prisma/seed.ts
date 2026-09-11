3dbf58b443a8225be02cb7b86499f92558f19a04
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-09-11 03:21:55 -0500

    feat(seeds): add workout target status seed and skip duplicates allowing to seed at any time

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,24 +5,26 @@ import { BODY_SECTIONS } from '../src/modules/muscle/seed/body-sections';
 import { MUSCLES } from '../src/modules/muscle/seed/muscles';
 import { MUSCULAR_GROUPS } from '../src/modules/muscle/seed/muscular-groups';
 import { STATUS } from '../src/modules/status/seed/status';
-import { WORKOUT_STATUS } from '../src/modules/tracking/seeds/tracking.seed';
+import { WORKOUT_STATUS, WORKOUT_TARGET_STATUS } from '../src/modules/tracking/seeds/tracking.seed';
 import { ROLES, USERS } from '../src/modules/user/seed/users';
 async function main() {
   await prisma.$transaction([
-    prisma.roles.createMany({ data: ROLES }),
-    prisma.users.createMany({ data: USERS }),
-    prisma.routineCycleTypes.createMany({ data: ROUTINE_CYCLES }),
-    prisma.status.createMany({ data: STATUS }),
-    prisma.dayTypes.createMany({ data: DAY_TYPES }),
-    prisma.bodySections.createMany({ data: BODY_SECTIONS }),
-    prisma.muscularGroups.createMany({ data: MUSCULAR_GROUPS }),
-    prisma.muscles.createMany({ data: MUSCLES }),
-    prisma.workoutStatus.createMany({ data: WORKOUT_STATUS }),
+    prisma.roles.createMany({ data: ROLES, skipDuplicates: true }),
+    prisma.users.createMany({ data: USERS, skipDuplicates: true }),
+    prisma.routineCycleTypes.createMany({ data: ROUTINE_CYCLES, skipDuplicates: true }),
+    prisma.status.createMany({ data: STATUS, skipDuplicates: true }),
+    prisma.dayTypes.createMany({ data: DAY_TYPES, skipDuplicates: true }),
+    prisma.bodySections.createMany({ data: BODY_SECTIONS, skipDuplicates: true }),
+    prisma.muscularGroups.createMany({ data: MUSCULAR_GROUPS, skipDuplicates: true }),
+    prisma.muscles.createMany({ data: MUSCLES, skipDuplicates: true }),
+    prisma.workoutStatus.createMany({ data: WORKOUT_STATUS, skipDuplicates: true }),
+    prisma.workoutTargetStatus.createMany({ data: WORKOUT_TARGET_STATUS, skipDuplicates: true }),
   ]);
 }
 
 main()
   .catch((error) => {
+    // biome-ignore lint/suspicious/noConsole: Server error log
     console.error(error);
     process.exit(1);
   })
--- src/modules/tracking/seeds/tracking.seed.ts
+++ src/modules/tracking/seeds/tracking.seed.ts
@@ -26,3 +26,26 @@ export const WORKOUT_STATUS: Prisma.WorkoutStatusCreateManyInput[] = [
     name: 'Completed',
   },
 ];
+
+export const WORKOUT_TARGET_STATUS: Prisma.WorkoutTargetStatusCreateManyInput[] = [
+  {
+    id: 'PENDING',
+    name: 'Pending',
+  },
+  {
+    id: 'IN_PROGRESS',
+    name: 'In progress',
+  },
+  {
+    id: 'SKIPPED',
+    name: 'Pending',
+  },
+  {
+    id: 'INTERRUPTED',
+    name: 'Interrupted',
+  },
+  {
+    id: 'COMPLETED',
+    name: 'Completed',
+  },
+];
